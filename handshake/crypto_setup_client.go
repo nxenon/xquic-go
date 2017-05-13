FMNSSun/quic-go

add a quic.Config option to ask the server to truncate the connection ID

handshake/crypto_setup_client.go
@@ -49,6 +49,7 @@ type cryptoSetupClient struct {
 	forwardSecureAEAD    crypto.AEAD
 	aeadChanged          chan<- protocol.EncryptionLevel
 
+	params               *TransportParameters
 	connectionParameters ConnectionParametersManager
 }
 
@@ -69,6 +70,7 @@ func NewCryptoSetupClient(
 	tlsConfig *tls.Config,
 	connectionParameters ConnectionParametersManager,
 	aeadChanged chan<- protocol.EncryptionLevel,
+	params *TransportParameters,
 	negotiatedVersions []protocol.VersionNumber,
 ) (CryptoSetup, error) {
 	return &cryptoSetupClient{
@@ -83,6 +85,7 @@ func NewCryptoSetupClient(
 		nullAEAD:             crypto.NewNullAEAD(protocol.PerspectiveClient, version),
 		aeadChanged:          aeadChanged,
 		negotiatedVersions:   negotiatedVersions,
+		params:               params,
 	}, nil
 }
 
@@ -415,10 +418,12 @@ func (h *cryptoSetupClient) getTags() (map[Tag][]byte, error) {
 	binary.LittleEndian.PutUint32(versionTag, protocol.VersionNumberToTag(h.version))
 	tags[TagVER] = versionTag
 
+	if h.params.TruncateConnectionID {
+		tags[TagTCID] = []byte{0, 0, 0, 0}
+	}
 	if len(h.stk) > 0 {
 		tags[TagSTK] = h.stk
 	}
-
 	if len(h.sno) > 0 {
 		tags[TagSNO] = h.sno
 	}

handshake/crypto_setup_client_test.go
@@ -105,7 +105,17 @@ var _ = Describe("Client Crypto Setup", func() {
 		certManager = &mockCertManager{}
 		version := protocol.Version36
 		aeadChanged = make(chan protocol.EncryptionLevel, 2)
-		csInt, err := NewCryptoSetupClient("hostname", 0, version, stream, nil, NewConnectionParamatersManager(protocol.PerspectiveClient, version), aeadChanged, nil)
+		csInt, err := NewCryptoSetupClient(
+			"hostname",
+			0,
+			version,
+			stream,
+			nil,
+			NewConnectionParamatersManager(protocol.PerspectiveClient, version),
+			aeadChanged,
+			&TransportParameters{},
+			nil,
+		)
 		Expect(err).ToNot(HaveOccurred())
 		cs = csInt.(*cryptoSetupClient)
 		cs.certManager = certManager
@@ -469,6 +479,14 @@ var _ = Describe("Client Crypto Setup", func() {
 			Expect(tags[TagPDMD]).To(Equal([]byte("X509")))
 			Expect(tags[TagVER]).To(Equal([]byte("Q036")))
 			Expect(tags[TagCCS]).To(Equal(certManager.commonCertificateHashes))
+			Expect(tags).ToNot(HaveKey(TagTCID))
+		})
+
+		It("requests to truncate the connection ID", func() {
+			cs.params.TruncateConnectionID = true
+			tags, err := cs.getTags()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(tags).To(HaveKeyWithValue(TagTCID, []byte{0, 0, 0, 0}))
 		})
 
 		It("adds the tags returned from the connectionParametersManager to the CHLO", func() {

session.go
@@ -170,7 +170,17 @@ func newClientSession(
 	s.aeadChanged = aeadChanged
 	cryptoStream, _ := s.OpenStream()
 	var err error
-	s.cryptoSetup, err = handshake.NewCryptoSetupClient(hostname, connectionID, v, cryptoStream, config.TLSConfig, s.connectionParameters, aeadChanged, negotiatedVersions)
+	s.cryptoSetup, err = handshake.NewCryptoSetupClient(
+		hostname,
+		connectionID,
+		v,
+		cryptoStream,
+		config.TLSConfig,
+		s.connectionParameters,
+		aeadChanged,
+		&handshake.TransportParameters{TruncateConnectionID: config.TruncateConnectionID},
+		negotiatedVersions,
+	)
 	if err != nil {
 		return nil, err
 	}

session_test.go
@@ -741,6 +741,20 @@ var _ = Describe("Session", func() {
 			Expect((*[]byte)(unsafe.Pointer(reflect.ValueOf(clientSess.cryptoSetup).Elem().FieldByName("diversificationNonce").UnsafeAddr()))).To(Equal(&hdr.DiversificationNonce))
 		})
 
+		It("passes the transport parameters to the cryptoSetup, as a client", func() {
+			s, err := newClientSession(
+				nil,
+				"hostname",
+				protocol.Version35,
+				0,
+				func(Session, bool) {},
+				populateClientConfig(&Config{TruncateConnectionID: true}),
+				nil,
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(*(*bool)(unsafe.Pointer(reflect.ValueOf(s.cryptoSetup).Elem().FieldByName("params").Elem().FieldByName("TruncateConnectionID").UnsafeAddr()))).To(BeTrue())
+		})
+
 		Context("updating the remote address", func() {
 			It("sets the remote address", func() {
 				remoteIP := &net.IPAddr{IP: net.IPv4(192, 168, 0, 100)}

nxenon/xquic-go

handshake/crypto_setup_client.go
@@ -51,6 +51,7 @@ type cryptoSetupClient struct {
 	forwardSecureAEAD    crypto.AEAD
 	aeadChanged          chan<- protocol.EncryptionLevel
 
+	params               *TransportParameters
 	connectionParameters ConnectionParametersManager
 }
 
@@ -71,6 +72,7 @@ func NewCryptoSetupClient(
 	tlsConfig *tls.Config,
 	connectionParameters ConnectionParametersManager,
 	aeadChanged chan<- protocol.EncryptionLevel,
+	params *TransportParameters,
 	negotiatedVersions []protocol.VersionNumber,
 ) (CryptoSetup, error) {
 	return &cryptoSetupClient{
@@ -86,6 +88,7 @@ func NewCryptoSetupClient(
 		aeadChanged:          aeadChanged,
 		negotiatedVersions:   negotiatedVersions,
 		divNonceChan:         make(chan []byte),
+		params:               params,
 	}, nil
 }
 
@@ -428,10 +431,12 @@ func (h *cryptoSetupClient) getTags() (map[Tag][]byte, error) {
 	binary.LittleEndian.PutUint32(versionTag, protocol.VersionNumberToTag(h.version))
 	tags[TagVER] = versionTag
 
+	if h.params.RequestConnectionIDTruncation {
+		tags[TagTCID] = []byte{0, 0, 0, 0}
+	}
 	if len(h.stk) > 0 {
 		tags[TagSTK] = h.stk
 	}
-
 	if len(h.sno) > 0 {
 		tags[TagSNO] = h.sno
 	}

handshake/crypto_setup_client_test.go
@@ -105,7 +105,17 @@ var _ = Describe("Client Crypto Setup", func() {
 		certManager = &mockCertManager{}
 		version := protocol.Version36
 		aeadChanged = make(chan protocol.EncryptionLevel, 2)
-		csInt, err := NewCryptoSetupClient("hostname", 0, version, stream, nil, NewConnectionParamatersManager(protocol.PerspectiveClient, version), aeadChanged, nil)
+		csInt, err := NewCryptoSetupClient(
+			"hostname",
+			0,
+			version,
+			stream,
+			nil,
+			NewConnectionParamatersManager(protocol.PerspectiveClient, version),
+			aeadChanged,
+			&TransportParameters{},
+			nil,
+		)
 		Expect(err).ToNot(HaveOccurred())
 		cs = csInt.(*cryptoSetupClient)
 		cs.certManager = certManager
@@ -468,6 +478,14 @@ var _ = Describe("Client Crypto Setup", func() {
 			Expect(tags[TagPDMD]).To(Equal([]byte("X509")))
 			Expect(tags[TagVER]).To(Equal([]byte("Q036")))
 			Expect(tags[TagCCS]).To(Equal(certManager.commonCertificateHashes))
+			Expect(tags).ToNot(HaveKey(TagTCID))
+		})
+
+		It("requests to truncate the connection ID", func() {
+			cs.params.RequestConnectionIDTruncation = true
+			tags, err := cs.getTags()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(tags).To(HaveKeyWithValue(TagTCID, []byte{0, 0, 0, 0}))
 		})
 
 		It("adds the tags returned from the connectionParametersManager to the CHLO", func() {

session.go
@@ -175,7 +175,17 @@ func newClientSession(
 	s.aeadChanged = aeadChanged
 	cryptoStream, _ := s.OpenStream()
 	var err error
-	s.cryptoSetup, err = handshake.NewCryptoSetupClient(hostname, connectionID, v, cryptoStream, config.TLSConfig, s.connectionParameters, aeadChanged, negotiatedVersions)
+	s.cryptoSetup, err = handshake.NewCryptoSetupClient(
+		hostname,
+		connectionID,
+		v,
+		cryptoStream,
+		config.TLSConfig,
+		s.connectionParameters,
+		aeadChanged,
+		&handshake.TransportParameters{RequestConnectionIDTruncation: config.RequestConnectionIDTruncation},
+		negotiatedVersions,
+	)
 	if err != nil {
 		return nil, err
 	}

session_test.go
@@ -749,6 +749,20 @@ var _ = Describe("Session", func() {
 			Expect(clientSess.Close(nil)).To(Succeed())
 		})
 
+		It("passes the transport parameters to the cryptoSetup, as a client", func() {
+			s, err := newClientSession(
+				nil,
+				"hostname",
+				protocol.Version35,
+				0,
+				func(Session, bool) {},
+				populateClientConfig(&Config{RequestConnectionIDTruncation: true}),
+				nil,
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(*(*bool)(unsafe.Pointer(reflect.ValueOf(s.cryptoSetup).Elem().FieldByName("params").Elem().FieldByName("RequestConnectionIDTruncation").UnsafeAddr()))).To(BeTrue())
+		})
+
 		Context("updating the remote address", func() {
 			It("sets the remote address", func() {
 				remoteIP := &net.IPAddr{IP: net.IPv4(192, 168, 0, 100)}