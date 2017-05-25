Acidburn0zzz/quic-go

add a quic.Config option to verify source address tokes

session.go
@@ -141,6 +141,15 @@ func newSession(
 	s.aeadChanged = aeadChanged
 	handshakeChan := make(chan handshakeEvent, 3)
 	s.handshakeChan = handshakeChan
+	verifySourceAddr := func(clientAddr net.Addr, hstk *handshake.STK) bool {
+		if hstk == nil {
+			return config.VerifySourceAddress(clientAddr, nil)
+		}
+		return config.VerifySourceAddress(
+			clientAddr,
+			&STK{remoteAddr: hstk.RemoteAddr, sentTime: hstk.SentTime},
+		)
+	}
 	var err error
 	s.cryptoSetup, err = newCryptoSetup(
 		connectionID,
@@ -150,6 +159,7 @@ func newSession(
 		cryptoStream,
 		s.connectionParameters,
 		config.Versions,
+		verifySourceAddr,
 		aeadChanged,
 	)
 	if err != nil {

session_test.go
@@ -145,6 +145,7 @@ var _ = Describe("Session", func() {
 			_ io.ReadWriter,
 			_ handshake.ConnectionParametersManager,
 			_ []protocol.VersionNumber,
+			_ func(net.Addr, *handshake.STK) bool,
 			aeadChangedP chan<- protocol.EncryptionLevel,
 		) (handshake.CryptoSetup, error) {
 			aeadChanged = aeadChangedP
@@ -178,6 +179,64 @@ var _ = Describe("Session", func() {
 	AfterEach(func() {
 		newCryptoSetup = handshake.NewCryptoSetup
 		Eventually(areSessionsRunning).Should(BeFalse())
+	})
+
+	Context("source address validation", func() {
+		var (
+			stkVerify       func(net.Addr, *handshake.STK) bool
+			paramClientAddr net.Addr
+			paramSTK        *STK
+		)
+		remoteAddr := &net.UDPAddr{IP: net.IPv4(192, 168, 13, 37), Port: 1000}
+
+		BeforeEach(func() {
+			newCryptoSetup = func(
+				_ protocol.ConnectionID,
+				_ net.Addr,
+				_ protocol.VersionNumber,
+				_ *handshake.ServerConfig,
+				_ io.ReadWriter,
+				_ handshake.ConnectionParametersManager,
+				_ []protocol.VersionNumber,
+				stkFunc func(net.Addr, *handshake.STK) bool,
+				_ chan<- protocol.EncryptionLevel,
+			) (handshake.CryptoSetup, error) {
+				stkVerify = stkFunc
+				return cryptoSetup, nil
+			}
+
+			conf := populateServerConfig(&Config{})
+			conf.VerifySourceAddress = func(clientAddr net.Addr, stk *STK) bool {
+				paramClientAddr = clientAddr
+				paramSTK = stk
+				return true
+			}
+			pSess, _, err := newSession(
+				mconn,
+				protocol.Version35,
+				0,
+				scfg,
+				conf,
+			)
+			Expect(err).NotTo(HaveOccurred())
+			sess = pSess.(*session)
+		})
+
+		It("calls the callback with the right parameters when the client didn't send an STK", func() {
+			stkVerify(remoteAddr, nil)
+			Expect(paramClientAddr).To(Equal(remoteAddr))
+			Expect(paramSTK).To(BeNil())
+		})
+
+		It("calls the callback with the STK when the client sent an STK", func() {
+			stkAddr := &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 1337}
+			sentTime := time.Now().Add(-time.Hour)
+			stkVerify(remoteAddr, &handshake.STK{SentTime: sentTime, RemoteAddr: stkAddr.String()})
+			Expect(paramClientAddr).To(Equal(remoteAddr))
+			Expect(paramSTK).ToNot(BeNil())
+			Expect(paramSTK.remoteAddr).To(Equal(stkAddr.String()))
+			Expect(paramSTK.sentTime).To(Equal(sentTime))
+		})
 	})
 
 	Context("when handling stream frames", func() {

nxenon/xquic-go

session.go
@@ -141,6 +141,15 @@ func newSession(
 	s.aeadChanged = aeadChanged
 	handshakeChan := make(chan handshakeEvent, 3)
 	s.handshakeChan = handshakeChan
+	verifySourceAddr := func(clientAddr net.Addr, hstk *handshake.STK) bool {
+		if hstk == nil {
+			return config.AcceptSTK(clientAddr, nil)
+		}
+		return config.AcceptSTK(
+			clientAddr,
+			&STK{remoteAddr: hstk.RemoteAddr, sentTime: hstk.SentTime},
+		)
+	}
 	var err error
 	s.cryptoSetup, err = newCryptoSetup(
 		connectionID,
@@ -150,6 +159,7 @@ func newSession(
 		cryptoStream,
 		s.connectionParameters,
 		config.Versions,
+		verifySourceAddr,
 		aeadChanged,
 	)
 	if err != nil {

session_test.go
@@ -145,6 +145,7 @@ var _ = Describe("Session", func() {
 			_ io.ReadWriter,
 			_ handshake.ConnectionParametersManager,
 			_ []protocol.VersionNumber,
+			_ func(net.Addr, *handshake.STK) bool,
 			aeadChangedP chan<- protocol.EncryptionLevel,
 		) (handshake.CryptoSetup, error) {
 			aeadChanged = aeadChangedP
@@ -178,6 +179,64 @@ var _ = Describe("Session", func() {
 	AfterEach(func() {
 		newCryptoSetup = handshake.NewCryptoSetup
 		Eventually(areSessionsRunning).Should(BeFalse())
+	})
+
+	Context("source address validation", func() {
+		var (
+			stkVerify       func(net.Addr, *handshake.STK) bool
+			paramClientAddr net.Addr
+			paramSTK        *STK
+		)
+		remoteAddr := &net.UDPAddr{IP: net.IPv4(192, 168, 13, 37), Port: 1000}
+
+		BeforeEach(func() {
+			newCryptoSetup = func(
+				_ protocol.ConnectionID,
+				_ net.Addr,
+				_ protocol.VersionNumber,
+				_ *handshake.ServerConfig,
+				_ io.ReadWriter,
+				_ handshake.ConnectionParametersManager,
+				_ []protocol.VersionNumber,
+				stkFunc func(net.Addr, *handshake.STK) bool,
+				_ chan<- protocol.EncryptionLevel,
+			) (handshake.CryptoSetup, error) {
+				stkVerify = stkFunc
+				return cryptoSetup, nil
+			}
+
+			conf := populateServerConfig(&Config{})
+			conf.AcceptSTK = func(clientAddr net.Addr, stk *STK) bool {
+				paramClientAddr = clientAddr
+				paramSTK = stk
+				return false
+			}
+			pSess, _, err := newSession(
+				mconn,
+				protocol.Version35,
+				0,
+				scfg,
+				conf,
+			)
+			Expect(err).NotTo(HaveOccurred())
+			sess = pSess.(*session)
+		})
+
+		It("calls the callback with the right parameters when the client didn't send an STK", func() {
+			stkVerify(remoteAddr, nil)
+			Expect(paramClientAddr).To(Equal(remoteAddr))
+			Expect(paramSTK).To(BeNil())
+		})
+
+		It("calls the callback with the STK when the client sent an STK", func() {
+			stkAddr := &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 1337}
+			sentTime := time.Now().Add(-time.Hour)
+			stkVerify(remoteAddr, &handshake.STK{SentTime: sentTime, RemoteAddr: stkAddr.String()})
+			Expect(paramClientAddr).To(Equal(remoteAddr))
+			Expect(paramSTK).ToNot(BeNil())
+			Expect(paramSTK.remoteAddr).To(Equal(stkAddr.String()))
+			Expect(paramSTK.sentTime).To(Equal(sentTime))
+		})
 	})
 
 	Context("when handling stream frames", func() {