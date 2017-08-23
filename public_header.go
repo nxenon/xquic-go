glycerine/quic-go

implement a function to parse the connection ID from the Public Header
This will be needed to correctly parse the Public Header later. With the
upcoming changes in Q039 (and later, the transition to the IETF header
format), it's necessary to know the QUIC version in order to parse the
Public Header. For packets sent after the handshake, the version field
is not present in the Public Header, so we have to look it up by the
connection ID.

public_header.go
@@ -109,6 +109,35 @@ func (h *PublicHeader) Write(b *bytes.Buffer, version protocol.VersionNumber, pe
 	return nil
 }
 
+// ParseConnectionID parses the connection ID from a QUIC packet's public header.
+// It no error occurs, it restores the read position in the bytes.Reader.
+func ParseConnectionID(b *bytes.Reader, packetSentBy protocol.Perspective) (protocol.ConnectionID, error) {
+	var connectionID protocol.ConnectionID
+	publicFlagByte, err := b.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	// unread the public flag byte
+	defer b.UnreadByte()
+
+	truncateConnectionID := publicFlagByte&0x08 == 0
+	if truncateConnectionID && packetSentBy == protocol.PerspectiveClient {
+		return 0, errReceivedTruncatedConnectionID
+	}
+	if !truncateConnectionID {
+		connID, err := utils.LittleEndian.ReadUint64(b)
+		if err != nil {
+			return 0, err
+		}
+		connectionID = protocol.ConnectionID(connID)
+		// unread the connection ID
+		for i := 0; i < 8; i++ {
+			b.UnreadByte()
+		}
+	}
+	return connectionID, nil
+}
+
 // ParsePublicHeader parses a QUIC packet's public header.
 // The packetSentBy is the perspective of the peer that sent this PublicHeader, i.e. if we're the server, packetSentBy should be PerspectiveClient.
 // Warning: This API should not be considered stable and will change soon.

public_header_test.go
@@ -11,6 +11,44 @@ import (
 )
 
 var _ = Describe("Public Header", func() {
+	Context("parsing the connection ID", func() {
+		It("does not accept truncated connection ID as a server", func() {
+			b := bytes.NewReader([]byte{0x00, 0x01})
+			_, err := ParseConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(MatchError(errReceivedTruncatedConnectionID))
+		})
+
+		It("gets the connection ID", func() {
+			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b, 0x9f, 0xfa, 0x4c, 0x51, 0x30, 0x33, 0x34, 0x01})
+			len := b.Len()
+			connID, err := ParseConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(connID).To(Equal(protocol.ConnectionID(0x4cfa9f9b668619f6)))
+			Expect(b.Len()).To(Equal(len))
+		})
+
+		It("errors if the Public Header is too short", func() {
+			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b})
+			_, err := ParseConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("errors if the Public Header is empty", func() {
+			b := bytes.NewReader([]byte{})
+			_, err := ParseConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("accepts a truncated connection ID as a client", func() {
+			b := bytes.NewReader([]byte{0x00, 0x01})
+			len := b.Len()
+			connID, err := ParseConnectionID(b, protocol.PerspectiveServer)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(connID).To(BeZero())
+			Expect(b.Len()).To(Equal(len))
+		})
+	})
+
 	Context("when parsing", func() {
 		It("accepts a sample client header", func() {
 			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b, 0x9f, 0xfa, 0x4c, 0x51, 0x30, 0x33, 0x34, 0x01})

nxenon/xquic-go

public_header.go
@@ -110,6 +110,35 @@ func (h *PublicHeader) Write(b *bytes.Buffer, version protocol.VersionNumber, pe
 	return nil
 }
 
+// PeekConnectionID parses the connection ID from a QUIC packet's public header.
+// If no error occurs, it restores the read position in the bytes.Reader.
+func PeekConnectionID(b *bytes.Reader, packetSentBy protocol.Perspective) (protocol.ConnectionID, error) {
+	var connectionID protocol.ConnectionID
+	publicFlagByte, err := b.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	// unread the public flag byte
+	defer b.UnreadByte()
+
+	truncateConnectionID := publicFlagByte&0x08 == 0
+	if truncateConnectionID && packetSentBy == protocol.PerspectiveClient {
+		return 0, errReceivedTruncatedConnectionID
+	}
+	if !truncateConnectionID {
+		connID, err := utils.LittleEndian.ReadUint64(b)
+		if err != nil {
+			return 0, err
+		}
+		connectionID = protocol.ConnectionID(connID)
+		// unread the connection ID
+		for i := 0; i < 8; i++ {
+			b.UnreadByte()
+		}
+	}
+	return connectionID, nil
+}
+
 // ParsePublicHeader parses a QUIC packet's public header.
 // The packetSentBy is the perspective of the peer that sent this PublicHeader, i.e. if we're the server, packetSentBy should be PerspectiveClient.
 // Warning: This API should not be considered stable and will change soon.

public_header_test.go
@@ -11,6 +11,44 @@ import (
 )
 
 var _ = Describe("Public Header", func() {
+	Context("parsing the connection ID", func() {
+		It("does not accept truncated connection ID as a server", func() {
+			b := bytes.NewReader([]byte{0x00, 0x01})
+			_, err := PeekConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(MatchError(errReceivedTruncatedConnectionID))
+		})
+
+		It("gets the connection ID", func() {
+			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b, 0x9f, 0xfa, 0x4c, 0x51, 0x30, 0x33, 0x34, 0x01})
+			len := b.Len()
+			connID, err := PeekConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(connID).To(Equal(protocol.ConnectionID(0x4cfa9f9b668619f6)))
+			Expect(b.Len()).To(Equal(len))
+		})
+
+		It("errors if the Public Header is too short", func() {
+			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b})
+			_, err := PeekConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("errors if the Public Header is empty", func() {
+			b := bytes.NewReader([]byte{})
+			_, err := PeekConnectionID(b, protocol.PerspectiveClient)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("accepts a truncated connection ID as a client", func() {
+			b := bytes.NewReader([]byte{0x00, 0x01})
+			len := b.Len()
+			connID, err := PeekConnectionID(b, protocol.PerspectiveServer)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(connID).To(BeZero())
+			Expect(b.Len()).To(Equal(len))
+		})
+	})
+
 	Context("when parsing", func() {
 		It("accepts a sample client header", func() {
 			b := bytes.NewReader([]byte{0x09, 0xf6, 0x19, 0x86, 0x66, 0x9b, 0x9f, 0xfa, 0x4c, 0x51, 0x30, 0x33, 0x34, 0x01})