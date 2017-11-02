zizhong/quic-go

implement the TLS Cookie extension

handshake/crypto_setup_tls.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"sync"
 
 	"github.com/bifurcation/mint"
@@ -24,19 +25,23 @@ type cryptoSetupTLS struct {
 	mintConf *mint.Config
 	conn     crypto.MintController
 
+	checkCookieCallback func(net.Addr, *Cookie) bool
+
 	nullAEAD crypto.AEAD
 	aead     crypto.AEAD
 
 	aeadChanged chan<- protocol.EncryptionLevel
 }
 
-// NewCryptoSetupTLS creates a new CryptoSetup instance for a server
+// NewCryptoSetupTLS creates a new CryptoSetup instance
 func NewCryptoSetupTLS(
 	hostname string, // only needed for the client
+	remoteAddr net.Addr,
 	perspective protocol.Perspective,
 	version protocol.VersionNumber,
 	tlsConfig *tls.Config,
 	cryptoStream io.ReadWriter,
+	checkCookie func(net.Addr, *Cookie) bool,
 	aeadChanged chan<- protocol.EncryptionLevel,
 ) (CryptoSetup, error) {
 	mintConf, err := tlsToMintConfig(tlsConfig, perspective)
@@ -44,11 +49,21 @@ func NewCryptoSetupTLS(
 		return nil, err
 	}
 	mintConf.ServerName = hostname
+	fc := &fakeConn{
+		ReadWriter: cryptoStream,
+		remoteAddr: remoteAddr,
+	}
 	var conn *mint.Conn
 	if perspective == protocol.PerspectiveServer {
-		conn = mint.Server(&fakeConn{cryptoStream}, mintConf)
+		var err error
+		mintConf.RequireCookie = true
+		mintConf.CookieHandler, err = newCookieHandler(checkCookie)
+		if err != nil {
+			return nil, err
+		}
+		conn = mint.Server(fc, mintConf)
 	} else {
-		conn = mint.Client(&fakeConn{cryptoStream}, mintConf)
+		conn = mint.Client(fc, mintConf)
 	}
 	return &cryptoSetupTLS{
 		perspective:   perspective,

nxenon/xquic-go

internal/handshake/crypto_setup_tls.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"sync"
 
 	"github.com/bifurcation/mint"
@@ -36,9 +37,11 @@ func NewCryptoSetupTLSServer(
 	cryptoStream io.ReadWriter,
 	connID protocol.ConnectionID,
 	tlsConfig *tls.Config,
+	remoteAddr net.Addr,
 	params *TransportParameters,
 	paramsChan chan<- TransportParameters,
 	aeadChanged chan<- protocol.EncryptionLevel,
+	checkCookie func(net.Addr, *Cookie) bool,
 	supportedVersions []protocol.VersionNumber,
 	version protocol.VersionNumber,
 ) (CryptoSetup, error) {
@@ -46,7 +49,16 @@ func NewCryptoSetupTLSServer(
 	if err != nil {
 		return nil, err
 	}
-	conn := &fakeConn{stream: cryptoStream, pers: protocol.PerspectiveServer}
+	mintConf.RequireCookie = true
+	mintConf.CookieHandler, err = newCookieHandler(checkCookie)
+	if err != nil {
+		return nil, err
+	}
+	conn := &fakeConn{
+		stream:     cryptoStream,
+		pers:       protocol.PerspectiveServer,
+		remoteAddr: remoteAddr,
+	}
 	mintConn := mint.Server(conn, mintConf)
 	eh := newExtensionHandlerServer(params, paramsChan, supportedVersions, version)
 	if err := mintConn.SetExtensionHandler(eh); err != nil {
@@ -86,7 +98,10 @@ func NewCryptoSetupTLSClient(
 		return nil, err
 	}
 	mintConf.ServerName = hostname
-	conn := &fakeConn{stream: cryptoStream, pers: protocol.PerspectiveClient}
+	conn := &fakeConn{
+		stream: cryptoStream,
+		pers:   protocol.PerspectiveClient,
+	}
 	mintConn := mint.Client(conn, mintConf)
 	eh := newExtensionHandlerClient(params, paramsChan, initialVersion, supportedVersions, version)
 	if err := mintConn.SetExtensionHandler(eh); err != nil {