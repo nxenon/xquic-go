heri16/quic-go

implement the MAX_DATA and MAX_STREAM_DATA frames
For gQUIC WINDOW_UPDATEs are converted to MAX_DATA and MAX_STREAM_DATA,
respectively.

internal/wire/max_data_frame_test.go
@@ -1,0 +1,56 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MAX_DATA frame", func() {
+	Context("when parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x4,
+				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
+			})
+			frame, err := ParseMaxDataFrame(b, versionBigEndian)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x4,
+				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
+			}
+			_, err := ParseMaxDataFrame(bytes.NewReader(data), versionCryptoStream0)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseMaxDataFrame(bytes.NewReader(data[0:i]), versionCryptoStream0)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &MaxDataFrame{
+				ByteOffset: 0xdeadbeef,
+			}
+			Expect(f.MinLength(versionCryptoStream0)).To(Equal(protocol.ByteCount(1 + 8)))
+		})
+
+		It("writes a MAX_DATA frame", func() {
+			b := &bytes.Buffer{}
+			f := &MaxDataFrame{
+				ByteOffset: 0xdeadbeefcafe1337,
+			}
+			err := f.Write(b, versionCryptoStream0)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x4,
+				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
+			}))
+		})
+	})
+})

internal/wire/max_stream_data_frame_test.go
@@ -1,0 +1,62 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MAX_STREAM_DATA frame", func() {
+	Context("parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x5,
+				0xde, 0xad, 0xbe, 0xef, // stream id
+				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
+			})
+			frame, err := ParseMaxStreamDataFrame(b, versionCryptoStream0)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x5,
+				0xef, 0xbe, 0xad, 0xde, // stream id
+				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
+			}
+			_, err := ParseMaxStreamDataFrame(bytes.NewReader(data), versionCryptoStream0)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseMaxStreamDataFrame(bytes.NewReader(data[0:i]), versionCryptoStream0)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &MaxStreamDataFrame{
+				StreamID:   0x1337,
+				ByteOffset: 0xdeadbeef,
+			}
+			Expect(f.MinLength(0)).To(Equal(protocol.ByteCount(13)))
+		})
+
+		It("writes a sample frame", func() {
+			b := &bytes.Buffer{}
+			f := &MaxStreamDataFrame{
+				StreamID:   0xdecafbad,
+				ByteOffset: 0xdeadbeefcafe1337,
+			}
+			err := f.Write(b, versionCryptoStream0)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x5,
+				0xde, 0xca, 0xfb, 0xad, // stream id
+				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
+			}))
+		})
+	})
+})

nxenon/xquic-go

internal/wire/max_data_frame_test.go
@@ -1,0 +1,56 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MAX_DATA frame", func() {
+	Context("when parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x4,
+				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
+			})
+			frame, err := ParseMaxDataFrame(b, versionBigEndian)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x4,
+				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
+			}
+			_, err := ParseMaxDataFrame(bytes.NewReader(data), versionMaxDataFrame)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseMaxDataFrame(bytes.NewReader(data[0:i]), versionMaxDataFrame)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &MaxDataFrame{
+				ByteOffset: 0xdeadbeef,
+			}
+			Expect(f.MinLength(versionMaxDataFrame)).To(Equal(protocol.ByteCount(1 + 8)))
+		})
+
+		It("writes a MAX_DATA frame", func() {
+			b := &bytes.Buffer{}
+			f := &MaxDataFrame{
+				ByteOffset: 0xdeadbeefcafe1337,
+			}
+			err := f.Write(b, versionMaxDataFrame)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x4,
+				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
+			}))
+		})
+	})
+})

internal/wire/max_stream_data_frame_test.go
@@ -1,0 +1,62 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MAX_STREAM_DATA frame", func() {
+	Context("parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x5,
+				0xde, 0xad, 0xbe, 0xef, // stream id
+				0xde, 0xca, 0xfb, 0xad, 0x11, 0x22, 0x33, 0x44, // byte offset
+			})
+			frame, err := ParseMaxStreamDataFrame(b, versionMaxDataFrame)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
+			Expect(frame.ByteOffset).To(Equal(protocol.ByteCount(0xdecafbad11223344)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x5,
+				0xef, 0xbe, 0xad, 0xde, // stream id
+				0x44, 0x33, 0x22, 0x11, 0xad, 0xfb, 0xca, 0xde, // byte offset
+			}
+			_, err := ParseMaxStreamDataFrame(bytes.NewReader(data), versionMaxDataFrame)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseMaxStreamDataFrame(bytes.NewReader(data[0:i]), versionMaxDataFrame)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &MaxStreamDataFrame{
+				StreamID:   0x1337,
+				ByteOffset: 0xdeadbeef,
+			}
+			Expect(f.MinLength(0)).To(Equal(protocol.ByteCount(13)))
+		})
+
+		It("writes a sample frame", func() {
+			b := &bytes.Buffer{}
+			f := &MaxStreamDataFrame{
+				StreamID:   0xdecafbad,
+				ByteOffset: 0xdeadbeefcafe1337,
+			}
+			err := f.Write(b, versionMaxDataFrame)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x5,
+				0xde, 0xca, 0xfb, 0xad, // stream id
+				0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0x13, 0x37, // byte offset
+			}))
+		})
+	})
+})