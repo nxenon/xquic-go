heri16/quic-go

implement the IETF draft BLOCKED and STREAM_BLOCKED frames

internal/wire/blocked_frame_test.go
@@ -4,70 +4,38 @@ import (
 	"bytes"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("BlockedFrame", func() {
+var _ = Describe("BLOCKED frame", func() {
 	Context("when parsing", func() {
-		Context("in little endian", func() {
-			It("accepts sample frame", func() {
-				b := bytes.NewReader([]byte{0x5, 0xef, 0xbe, 0xad, 0xde})
-				frame, err := ParseBlockedFrame(b, versionLittleEndian)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			})
-		})
-
-		Context("in big endian", func() {
-			It("accepts sample frame", func() {
-				b := bytes.NewReader([]byte{0x5, 0xde, 0xad, 0xbe, 0xef})
-				frame, err := ParseBlockedFrame(b, versionBigEndian)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			})
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x08})
+			_, err := ParseBlockedFrame(b, protocol.VersionWhatever)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Len()).To(BeZero())
 		})
 
 		It("errors on EOFs", func() {
-			data := []byte{0x5, 0xef, 0xbe, 0xad, 0xde}
-			_, err := ParseBlockedFrame(bytes.NewReader(data), protocol.VersionWhatever)
-			Expect(err).NotTo(HaveOccurred())
-			for i := range data {
-				_, err := ParseBlockedFrame(bytes.NewReader(data[0:i]), protocol.VersionWhatever)
-				Expect(err).To(HaveOccurred())
-			}
+			_, err := ParseBlockedFrame(bytes.NewReader(nil), protocol.VersionWhatever)
+			Expect(err).To(HaveOccurred())
 		})
 	})
 
 	Context("when writing", func() {
-		Context("in little endian", func() {
-			It("writes a sample frame", func() {
-				b := &bytes.Buffer{}
-				frame := BlockedFrame{StreamID: 0x1337}
-				frame.Write(b, versionLittleEndian)
-				Expect(b.Bytes()).To(Equal([]byte{0x5, 0x37, 0x13, 0x0, 0x0}))
-			})
-		})
-
-		Context("in big endian", func() {
-			It("writes a sample frame", func() {
-				b := &bytes.Buffer{}
-				frame := BlockedFrame{StreamID: 0x1337}
-				frame.Write(b, versionBigEndian)
-				Expect(b.Bytes()).To(Equal([]byte{0x5, 0x0, 0x0, 0x13, 0x37}))
-			})
-		})
-
-		It("writes a connection-level Blocked", func() {
+		It("writes a sample frame", func() {
 			b := &bytes.Buffer{}
-			frame := BlockedFrame{StreamID: 0}
-			frame.Write(b, 0)
-			Expect(b.Bytes()).To(Equal([]byte{0x5, 0, 0, 0, 0}))
+			frame := BlockedFrame{}
+			err := frame.Write(b, protocol.VersionWhatever)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x08}))
 		})
 
 		It("has the correct min length", func() {
-			frame := BlockedFrame{StreamID: 3}
-			Expect(frame.MinLength(0)).To(Equal(protocol.ByteCount(5)))
+			frame := BlockedFrame{}
+			Expect(frame.MinLength(versionCryptoStream0)).To(Equal(protocol.ByteCount(1)))
 		})
 	})
 })

internal/wire/stream_blocked_frame_test.go
@@ -1,0 +1,57 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("STREAM_BLOCKED frame", func() {
+	Context("parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x9,
+				0xde, 0xad, 0xbe, 0xef, // stream id
+			})
+			frame, err := ParseStreamBlockedFrame(b, versionCryptoStream0)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x9,
+				0xef, 0xbe, 0xad, 0xde, // stream id
+			}
+			_, err := ParseStreamBlockedFrame(bytes.NewReader(data), versionCryptoStream0)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseStreamBlockedFrame(bytes.NewReader(data[0:i]), versionCryptoStream0)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &StreamBlockedFrame{
+				StreamID: 0x1337,
+			}
+			Expect(f.MinLength(0)).To(Equal(protocol.ByteCount(5)))
+		})
+
+		It("writes a sample frame", func() {
+			b := &bytes.Buffer{}
+			f := &StreamBlockedFrame{
+				StreamID: 0xdecafbad,
+			}
+			err := f.Write(b, versionCryptoStream0)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x9,
+				0xde, 0xca, 0xfb, 0xad, // stream id
+			}))
+		})
+	})
+})

nxenon/xquic-go

internal/wire/blocked_frame_test.go
@@ -4,70 +4,38 @@ import (
 	"bytes"
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("BlockedFrame", func() {
+var _ = Describe("BLOCKED frame", func() {
 	Context("when parsing", func() {
-		Context("in little endian", func() {
-			It("accepts sample frame", func() {
-				b := bytes.NewReader([]byte{0x5, 0xef, 0xbe, 0xad, 0xde})
-				frame, err := ParseBlockedFrame(b, versionLittleEndian)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			})
-		})
-
-		Context("in big endian", func() {
-			It("accepts sample frame", func() {
-				b := bytes.NewReader([]byte{0x5, 0xde, 0xad, 0xbe, 0xef})
-				frame, err := ParseBlockedFrame(b, versionBigEndian)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
-			})
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x08})
+			_, err := ParseBlockedFrame(b, protocol.VersionWhatever)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Len()).To(BeZero())
 		})
 
 		It("errors on EOFs", func() {
-			data := []byte{0x5, 0xef, 0xbe, 0xad, 0xde}
-			_, err := ParseBlockedFrame(bytes.NewReader(data), protocol.VersionWhatever)
-			Expect(err).NotTo(HaveOccurred())
-			for i := range data {
-				_, err := ParseBlockedFrame(bytes.NewReader(data[0:i]), protocol.VersionWhatever)
-				Expect(err).To(HaveOccurred())
-			}
+			_, err := ParseBlockedFrame(bytes.NewReader(nil), protocol.VersionWhatever)
+			Expect(err).To(HaveOccurred())
 		})
 	})
 
 	Context("when writing", func() {
-		Context("in little endian", func() {
-			It("writes a sample frame", func() {
-				b := &bytes.Buffer{}
-				frame := BlockedFrame{StreamID: 0x1337}
-				frame.Write(b, versionLittleEndian)
-				Expect(b.Bytes()).To(Equal([]byte{0x5, 0x37, 0x13, 0x0, 0x0}))
-			})
-		})
-
-		Context("in big endian", func() {
-			It("writes a sample frame", func() {
-				b := &bytes.Buffer{}
-				frame := BlockedFrame{StreamID: 0x1337}
-				frame.Write(b, versionBigEndian)
-				Expect(b.Bytes()).To(Equal([]byte{0x5, 0x0, 0x0, 0x13, 0x37}))
-			})
-		})
-
-		It("writes a connection-level Blocked", func() {
+		It("writes a sample frame", func() {
 			b := &bytes.Buffer{}
-			frame := BlockedFrame{StreamID: 0}
-			frame.Write(b, 0)
-			Expect(b.Bytes()).To(Equal([]byte{0x5, 0, 0, 0, 0}))
+			frame := BlockedFrame{}
+			err := frame.Write(b, protocol.VersionWhatever)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x08}))
 		})
 
 		It("has the correct min length", func() {
-			frame := BlockedFrame{StreamID: 3}
-			Expect(frame.MinLength(0)).To(Equal(protocol.ByteCount(5)))
+			frame := BlockedFrame{}
+			Expect(frame.MinLength(versionMaxDataFrame)).To(Equal(protocol.ByteCount(1)))
 		})
 	})
 })

internal/wire/stream_blocked_frame_test.go
@@ -1,0 +1,57 @@
+package wire
+
+import (
+	"bytes"
+
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("STREAM_BLOCKED frame", func() {
+	Context("parsing", func() {
+		It("accepts sample frame", func() {
+			b := bytes.NewReader([]byte{0x9,
+				0xde, 0xad, 0xbe, 0xef, // stream id
+			})
+			frame, err := ParseStreamBlockedFrame(b, versionMaxDataFrame)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(frame.StreamID).To(Equal(protocol.StreamID(0xdeadbeef)))
+			Expect(b.Len()).To(BeZero())
+		})
+
+		It("errors on EOFs", func() {
+			data := []byte{0x9,
+				0xef, 0xbe, 0xad, 0xde, // stream id
+			}
+			_, err := ParseStreamBlockedFrame(bytes.NewReader(data), versionMaxDataFrame)
+			Expect(err).NotTo(HaveOccurred())
+			for i := range data {
+				_, err := ParseStreamBlockedFrame(bytes.NewReader(data[0:i]), versionMaxDataFrame)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+
+	Context("writing", func() {
+		It("has proper min length", func() {
+			f := &StreamBlockedFrame{
+				StreamID: 0x1337,
+			}
+			Expect(f.MinLength(0)).To(Equal(protocol.ByteCount(5)))
+		})
+
+		It("writes a sample frame", func() {
+			b := &bytes.Buffer{}
+			f := &StreamBlockedFrame{
+				StreamID: 0xdecafbad,
+			}
+			err := f.Write(b, versionMaxDataFrame)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(b.Bytes()).To(Equal([]byte{0x9,
+				0xde, 0xca, 0xfb, 0xad, // stream id
+			}))
+		})
+	})
+})