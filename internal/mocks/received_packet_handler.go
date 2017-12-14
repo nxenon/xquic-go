yangxingwu/quic-go

use a gomock ReceivedPacketHandler in the session tests

internal/mocks/received_packet_handler.go
@@ -1,0 +1,82 @@
+// Code generated by MockGen. DO NOT EDIT.
+// Source: github.com/lucas-clemente/quic-go/ackhandler (interfaces: ReceivedPacketHandler)
+
+package mocks
+
+import (
+	reflect "reflect"
+	time "time"
+
+	gomock "github.com/golang/mock/gomock"
+	protocol "github.com/lucas-clemente/quic-go/internal/protocol"
+	wire "github.com/lucas-clemente/quic-go/internal/wire"
+)
+
+// MockReceivedPacketHandler is a mock of ReceivedPacketHandler interface
+type MockReceivedPacketHandler struct {
+	ctrl     *gomock.Controller
+	recorder *MockReceivedPacketHandlerMockRecorder
+}
+
+// MockReceivedPacketHandlerMockRecorder is the mock recorder for MockReceivedPacketHandler
+type MockReceivedPacketHandlerMockRecorder struct {
+	mock *MockReceivedPacketHandler
+}
+
+// NewMockReceivedPacketHandler creates a new mock instance
+func NewMockReceivedPacketHandler(ctrl *gomock.Controller) *MockReceivedPacketHandler {
+	mock := &MockReceivedPacketHandler{ctrl: ctrl}
+	mock.recorder = &MockReceivedPacketHandlerMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use
+func (_m *MockReceivedPacketHandler) EXPECT() *MockReceivedPacketHandlerMockRecorder {
+	return _m.recorder
+}
+
+// GetAckFrame mocks base method
+func (_m *MockReceivedPacketHandler) GetAckFrame() *wire.AckFrame {
+	ret := _m.ctrl.Call(_m, "GetAckFrame")
+	ret0, _ := ret[0].(*wire.AckFrame)
+	return ret0
+}
+
+// GetAckFrame indicates an expected call of GetAckFrame
+func (_mr *MockReceivedPacketHandlerMockRecorder) GetAckFrame() *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "GetAckFrame", reflect.TypeOf((*MockReceivedPacketHandler)(nil).GetAckFrame))
+}
+
+// GetAlarmTimeout mocks base method
+func (_m *MockReceivedPacketHandler) GetAlarmTimeout() time.Time {
+	ret := _m.ctrl.Call(_m, "GetAlarmTimeout")
+	ret0, _ := ret[0].(time.Time)
+	return ret0
+}
+
+// GetAlarmTimeout indicates an expected call of GetAlarmTimeout
+func (_mr *MockReceivedPacketHandlerMockRecorder) GetAlarmTimeout() *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "GetAlarmTimeout", reflect.TypeOf((*MockReceivedPacketHandler)(nil).GetAlarmTimeout))
+}
+
+// ReceivedPacket mocks base method
+func (_m *MockReceivedPacketHandler) ReceivedPacket(_param0 protocol.PacketNumber, _param1 bool) error {
+	ret := _m.ctrl.Call(_m, "ReceivedPacket", _param0, _param1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// ReceivedPacket indicates an expected call of ReceivedPacket
+func (_mr *MockReceivedPacketHandlerMockRecorder) ReceivedPacket(arg0, arg1 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "ReceivedPacket", reflect.TypeOf((*MockReceivedPacketHandler)(nil).ReceivedPacket), arg0, arg1)
+}
+
+// SetLowerLimit mocks base method
+func (_m *MockReceivedPacketHandler) SetLowerLimit(_param0 protocol.PacketNumber) {
+	_m.ctrl.Call(_m, "SetLowerLimit", _param0)
+}
+
+// SetLowerLimit indicates an expected call of SetLowerLimit
+func (_mr *MockReceivedPacketHandlerMockRecorder) SetLowerLimit(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "SetLowerLimit", reflect.TypeOf((*MockReceivedPacketHandler)(nil).SetLowerLimit), arg0)
+}

nxenon/xquic-go

internal/mocks/received_packet_handler.go
@@ -1,0 +1,82 @@
+// Code generated by MockGen. DO NOT EDIT.
+// Source: github.com/lucas-clemente/quic-go/ackhandler (interfaces: ReceivedPacketHandler)
+
+package mocks
+
+import (
+	reflect "reflect"
+	time "time"
+
+	gomock "github.com/golang/mock/gomock"
+	protocol "github.com/lucas-clemente/quic-go/internal/protocol"
+	wire "github.com/lucas-clemente/quic-go/internal/wire"
+)
+
+// MockReceivedPacketHandler is a mock of ReceivedPacketHandler interface
+type MockReceivedPacketHandler struct {
+	ctrl     *gomock.Controller
+	recorder *MockReceivedPacketHandlerMockRecorder
+}
+
+// MockReceivedPacketHandlerMockRecorder is the mock recorder for MockReceivedPacketHandler
+type MockReceivedPacketHandlerMockRecorder struct {
+	mock *MockReceivedPacketHandler
+}
+
+// NewMockReceivedPacketHandler creates a new mock instance
+func NewMockReceivedPacketHandler(ctrl *gomock.Controller) *MockReceivedPacketHandler {
+	mock := &MockReceivedPacketHandler{ctrl: ctrl}
+	mock.recorder = &MockReceivedPacketHandlerMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use
+func (_m *MockReceivedPacketHandler) EXPECT() *MockReceivedPacketHandlerMockRecorder {
+	return _m.recorder
+}
+
+// GetAckFrame mocks base method
+func (_m *MockReceivedPacketHandler) GetAckFrame() *wire.AckFrame {
+	ret := _m.ctrl.Call(_m, "GetAckFrame")
+	ret0, _ := ret[0].(*wire.AckFrame)
+	return ret0
+}
+
+// GetAckFrame indicates an expected call of GetAckFrame
+func (_mr *MockReceivedPacketHandlerMockRecorder) GetAckFrame() *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "GetAckFrame", reflect.TypeOf((*MockReceivedPacketHandler)(nil).GetAckFrame))
+}
+
+// GetAlarmTimeout mocks base method
+func (_m *MockReceivedPacketHandler) GetAlarmTimeout() time.Time {
+	ret := _m.ctrl.Call(_m, "GetAlarmTimeout")
+	ret0, _ := ret[0].(time.Time)
+	return ret0
+}
+
+// GetAlarmTimeout indicates an expected call of GetAlarmTimeout
+func (_mr *MockReceivedPacketHandlerMockRecorder) GetAlarmTimeout() *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "GetAlarmTimeout", reflect.TypeOf((*MockReceivedPacketHandler)(nil).GetAlarmTimeout))
+}
+
+// IgnoreBelow mocks base method
+func (_m *MockReceivedPacketHandler) IgnoreBelow(_param0 protocol.PacketNumber) {
+	_m.ctrl.Call(_m, "IgnoreBelow", _param0)
+}
+
+// IgnoreBelow indicates an expected call of IgnoreBelow
+func (_mr *MockReceivedPacketHandlerMockRecorder) IgnoreBelow(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "IgnoreBelow", reflect.TypeOf((*MockReceivedPacketHandler)(nil).IgnoreBelow), arg0)
+}
+
+// ReceivedPacket mocks base method
+func (_m *MockReceivedPacketHandler) ReceivedPacket(_param0 protocol.PacketNumber, _param1 bool) error {
+	ret := _m.ctrl.Call(_m, "ReceivedPacket", _param0, _param1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// ReceivedPacket indicates an expected call of ReceivedPacket
+func (_mr *MockReceivedPacketHandlerMockRecorder) ReceivedPacket(arg0, arg1 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCallWithMethodType(_mr.mock, "ReceivedPacket", reflect.TypeOf((*MockReceivedPacketHandler)(nil).ReceivedPacket), arg0, arg1)
+}