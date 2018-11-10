DrQuadCore/quic-go

rename the BLOCKED from to DATA_BLOCKED

framer_test.go
@@ -56,7 +56,7 @@ var _ = Describe("Stream Framer", func() {
 
 		It("adds the right number of frames", func() {
 			maxSize := protocol.ByteCount(1000)
-			bf := &wire.BlockedFrame{Offset: 0x1337}
+			bf := &wire.DataBlockedFrame{Offset: 0x1337}
 			bfLen := bf.Length(version)
 			numFrames := int(maxSize / bfLen) // max number of frames that fit into maxSize
 			for i := 0; i < numFrames+1; i++ {

session.go
@@ -556,7 +556,7 @@ func (s *session) handleFrames(fs []wire.Frame, encLevel protocol.EncryptionLeve
 			err = s.handleMaxStreamDataFrame(frame)
 		case *wire.MaxStreamIDFrame:
 			err = s.handleMaxStreamIDFrame(frame)
-		case *wire.BlockedFrame:
+		case *wire.DataBlockedFrame:
 		case *wire.StreamBlockedFrame:
 		case *wire.StreamIDBlockedFrame:
 		case *wire.StopSendingFrame:
@@ -898,7 +898,7 @@ func (s *session) sendProbePacket() error {
 
 func (s *session) sendPacket() (bool, error) {
 	if isBlocked, offset := s.connFlowController.IsNewlyBlocked(); isBlocked {
-		s.framer.QueueControlFrame(&wire.BlockedFrame{Offset: offset})
+		s.framer.QueueControlFrame(&wire.DataBlockedFrame{Offset: offset})
 	}
 	s.windowUpdateQueue.QueueAll()
 

session_test.go
@@ -307,7 +307,7 @@ var _ = Describe("Session", func() {
 		})
 
 		It("handles BLOCKED frames", func() {
-			err := sess.handleFrames([]wire.Frame{&wire.BlockedFrame{}}, protocol.EncryptionUnspecified)
+			err := sess.handleFrames([]wire.Frame{&wire.DataBlockedFrame{}}, protocol.EncryptionUnspecified)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -599,7 +599,7 @@ var _ = Describe("Session", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(sent).To(BeTrue())
 			frames, _ := sess.framer.AppendControlFrames(nil, 1000)
-			Expect(frames).To(Equal([]wire.Frame{&wire.BlockedFrame{Offset: 1337}}))
+			Expect(frames).To(Equal([]wire.Frame{&wire.DataBlockedFrame{Offset: 1337}}))
 		})
 
 		It("sends a retransmission and a regular packet in the same run", func() {

nxenon/xquic-go

framer_test.go
@@ -56,7 +56,7 @@ var _ = Describe("Stream Framer", func() {
 
 		It("adds the right number of frames", func() {
 			maxSize := protocol.ByteCount(1000)
-			bf := &wire.BlockedFrame{Offset: 0x1337}
+			bf := &wire.DataBlockedFrame{DataLimit: 0x1337}
 			bfLen := bf.Length(version)
 			numFrames := int(maxSize / bfLen) // max number of frames that fit into maxSize
 			for i := 0; i < numFrames+1; i++ {

session.go
@@ -551,7 +551,7 @@ func (s *session) handleFrames(fs []wire.Frame, encLevel protocol.EncryptionLeve
 			err = s.handleMaxStreamDataFrame(frame)
 		case *wire.MaxStreamIDFrame:
 			err = s.handleMaxStreamIDFrame(frame)
-		case *wire.BlockedFrame:
+		case *wire.DataBlockedFrame:
 		case *wire.StreamBlockedFrame:
 		case *wire.StreamIDBlockedFrame:
 		case *wire.StopSendingFrame:
@@ -893,7 +893,7 @@ func (s *session) sendProbePacket() error {
 
 func (s *session) sendPacket() (bool, error) {
 	if isBlocked, offset := s.connFlowController.IsNewlyBlocked(); isBlocked {
-		s.framer.QueueControlFrame(&wire.BlockedFrame{Offset: offset})
+		s.framer.QueueControlFrame(&wire.DataBlockedFrame{DataLimit: offset})
 	}
 	s.windowUpdateQueue.QueueAll()
 

session_test.go
@@ -306,7 +306,7 @@ var _ = Describe("Session", func() {
 		})
 
 		It("handles BLOCKED frames", func() {
-			err := sess.handleFrames([]wire.Frame{&wire.BlockedFrame{}}, protocol.EncryptionUnspecified)
+			err := sess.handleFrames([]wire.Frame{&wire.DataBlockedFrame{}}, protocol.EncryptionUnspecified)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -598,7 +598,7 @@ var _ = Describe("Session", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(sent).To(BeTrue())
 			frames, _ := sess.framer.AppendControlFrames(nil, 1000)
-			Expect(frames).To(Equal([]wire.Frame{&wire.BlockedFrame{Offset: 1337}}))
+			Expect(frames).To(Equal([]wire.Frame{&wire.DataBlockedFrame{DataLimit: 1337}}))
 		})
 
 		It("sends a retransmission and a regular packet in the same run", func() {