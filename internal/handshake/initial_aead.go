azurehongqiang/quic-go

refactor initialization of the initial AEAD

internal/handshake/initial_aead.go
@@ -1,0 +1,57 @@
+package handshake
+
+import (
+	gocrypto "crypto"
+	"crypto/aes"
+	"crypto/cipher"
+
+	"github.com/lucas-clemente/quic-go/internal/crypto"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+)
+
+var quicVersion1Salt = []byte{0x9c, 0x10, 0x8f, 0x98, 0x52, 0x0a, 0x5c, 0x5c, 0x32, 0x96, 0x8e, 0x95, 0x0e, 0x8a, 0x2c, 0x5f, 0xe0, 0x6d, 0x6c, 0x38}
+
+func newInitialAEAD(connID protocol.ConnectionID, pers protocol.Perspective) (Sealer, Opener, error) {
+	clientSecret, serverSecret := computeSecrets(connID)
+	var mySecret, otherSecret []byte
+	if pers == protocol.PerspectiveClient {
+		mySecret = clientSecret
+		otherSecret = serverSecret
+	} else {
+		mySecret = serverSecret
+		otherSecret = clientSecret
+	}
+	myKey, myIV := computeInitialDKeyAndIV(mySecret)
+	otherKey, otherIV := computeInitialDKeyAndIV(otherSecret)
+
+	encrypterCipher, err := aes.NewCipher(myKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	encrypter, err := cipher.NewGCM(encrypterCipher)
+	if err != nil {
+		return nil, nil, err
+	}
+	decrypterCipher, err := aes.NewCipher(otherKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	decrypter, err := cipher.NewGCM(decrypterCipher)
+	if err != nil {
+		return nil, nil, err
+	}
+	return newSealer(encrypter, myIV), newOpener(decrypter, otherIV), nil
+}
+
+func computeSecrets(connID protocol.ConnectionID) (clientSecret, serverSecret []byte) {
+	initialSecret := crypto.HkdfExtract(gocrypto.SHA256, connID, quicVersion1Salt)
+	clientSecret = crypto.HkdfExpandLabel(gocrypto.SHA256, initialSecret, "client in", gocrypto.SHA256.Size())
+	serverSecret = crypto.HkdfExpandLabel(gocrypto.SHA256, initialSecret, "server in", gocrypto.SHA256.Size())
+	return
+}
+
+func computeInitialDKeyAndIV(secret []byte) (key, iv []byte) {
+	key = crypto.HkdfExpandLabel(gocrypto.SHA256, secret, "key", 16)
+	iv = crypto.HkdfExpandLabel(gocrypto.SHA256, secret, "iv", 12)
+	return
+}

internal/handshake/initial_aead_test.go
@@ -1,12 +1,13 @@
-package crypto
+package handshake
 
 import (
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("NullAEAD using AES-GCM", func() {
+var _ = Describe("Initial AEAD using AES-GCM", func() {
 	// values taken from https://github.com/quicwg/base-drafts/wiki/Test-Vector-for-the-Clear-Text-AEAD-key-derivation
 	Context("using the test vector from the QUIC WG Wiki", func() {
 		connID := protocol.ConnectionID([]byte{0x83, 0x94, 0xc8, 0xf0, 0x3e, 0x51, 0x57, 0x08})
@@ -29,7 +30,7 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 		It("computes the client key and IV", func() {
 			clientSecret, _ := computeSecrets(connID)
-			key, iv := computeNullAEADKeyAndIV(clientSecret)
+			key, iv := computeInitialDKeyAndIV(clientSecret)
 			Expect(key).To(Equal([]byte{
 				0xf2, 0x92, 0x8f, 0x26, 0x14, 0xad, 0x6c, 0x20,
 				0xb9, 0xbd, 0x00, 0x8e, 0x9c, 0x89, 0x63, 0x1c,
@@ -42,7 +43,7 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 		It("computes the server key and IV", func() {
 			_, serverSecret := computeSecrets(connID)
-			key, iv := computeNullAEADKeyAndIV(serverSecret)
+			key, iv := computeInitialDKeyAndIV(serverSecret)
 			Expect(key).To(Equal([]byte{
 				0xf5, 0x68, 0x17, 0xd0, 0xfc, 0x59, 0x5c, 0xfc,
 				0x0a, 0x2b, 0x0b, 0xcf, 0xb1, 0x87, 0x35, 0xec,
@@ -56,17 +57,17 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 	It("seals and opens", func() {
 		connectionID := protocol.ConnectionID([]byte{0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef})
-		clientAEAD, err := NewNullAEAD(connectionID, protocol.PerspectiveClient)
+		clientSealer, clientOpener, err := newInitialAEAD(connectionID, protocol.PerspectiveClient)
 		Expect(err).ToNot(HaveOccurred())
-		serverAEAD, err := NewNullAEAD(connectionID, protocol.PerspectiveServer)
+		serverSealer, serverOpener, err := newInitialAEAD(connectionID, protocol.PerspectiveServer)
 		Expect(err).ToNot(HaveOccurred())
 
-		clientMessage := clientAEAD.Seal(nil, []byte("foobar"), 42, []byte("aad"))
-		m, err := serverAEAD.Open(nil, clientMessage, 42, []byte("aad"))
+		clientMessage := clientSealer.Seal(nil, []byte("foobar"), 42, []byte("aad"))
+		m, err := serverOpener.Open(nil, clientMessage, 42, []byte("aad"))
 		Expect(err).ToNot(HaveOccurred())
 		Expect(m).To(Equal([]byte("foobar")))
-		serverMessage := serverAEAD.Seal(nil, []byte("raboof"), 99, []byte("daa"))
-		m, err = clientAEAD.Open(nil, serverMessage, 99, []byte("daa"))
+		serverMessage := serverSealer.Seal(nil, []byte("raboof"), 99, []byte("daa"))
+		m, err = clientOpener.Open(nil, serverMessage, 99, []byte("daa"))
 		Expect(err).ToNot(HaveOccurred())
 		Expect(m).To(Equal([]byte("raboof")))
 	})
@@ -74,13 +75,13 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 	It("doesn't work if initialized with different connection IDs", func() {
 		c1 := protocol.ConnectionID([]byte{0, 0, 0, 0, 0, 0, 0, 1})
 		c2 := protocol.ConnectionID([]byte{0, 0, 0, 0, 0, 0, 0, 2})
-		clientAEAD, err := NewNullAEAD(c1, protocol.PerspectiveClient)
+		clientSealer, _, err := newInitialAEAD(c1, protocol.PerspectiveClient)
 		Expect(err).ToNot(HaveOccurred())
-		serverAEAD, err := NewNullAEAD(c2, protocol.PerspectiveServer)
+		_, serverOpener, err := newInitialAEAD(c2, protocol.PerspectiveServer)
 		Expect(err).ToNot(HaveOccurred())
 
-		clientMessage := clientAEAD.Seal(nil, []byte("foobar"), 42, []byte("aad"))
-		_, err = serverAEAD.Open(nil, clientMessage, 42, []byte("aad"))
+		clientMessage := clientSealer.Seal(nil, []byte("foobar"), 42, []byte("aad"))
+		_, err = serverOpener.Open(nil, clientMessage, 42, []byte("aad"))
 		Expect(err).To(MatchError("cipher: message authentication failed"))
 	})
 })

nxenon/xquic-go

internal/handshake/initial_aead.go
@@ -1,0 +1,57 @@
+package handshake
+
+import (
+	gocrypto "crypto"
+	"crypto/aes"
+	"crypto/cipher"
+
+	"github.com/lucas-clemente/quic-go/internal/crypto"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+)
+
+var quicVersion1Salt = []byte{0x9c, 0x10, 0x8f, 0x98, 0x52, 0x0a, 0x5c, 0x5c, 0x32, 0x96, 0x8e, 0x95, 0x0e, 0x8a, 0x2c, 0x5f, 0xe0, 0x6d, 0x6c, 0x38}
+
+func newInitialAEAD(connID protocol.ConnectionID, pers protocol.Perspective) (Sealer, Opener, error) {
+	clientSecret, serverSecret := computeSecrets(connID)
+	var mySecret, otherSecret []byte
+	if pers == protocol.PerspectiveClient {
+		mySecret = clientSecret
+		otherSecret = serverSecret
+	} else {
+		mySecret = serverSecret
+		otherSecret = clientSecret
+	}
+	myKey, myIV := computeInitialKeyAndIV(mySecret)
+	otherKey, otherIV := computeInitialKeyAndIV(otherSecret)
+
+	encrypterCipher, err := aes.NewCipher(myKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	encrypter, err := cipher.NewGCM(encrypterCipher)
+	if err != nil {
+		return nil, nil, err
+	}
+	decrypterCipher, err := aes.NewCipher(otherKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	decrypter, err := cipher.NewGCM(decrypterCipher)
+	if err != nil {
+		return nil, nil, err
+	}
+	return newSealer(encrypter, myIV), newOpener(decrypter, otherIV), nil
+}
+
+func computeSecrets(connID protocol.ConnectionID) (clientSecret, serverSecret []byte) {
+	initialSecret := crypto.HkdfExtract(gocrypto.SHA256, connID, quicVersion1Salt)
+	clientSecret = crypto.HkdfExpandLabel(gocrypto.SHA256, initialSecret, "client in", gocrypto.SHA256.Size())
+	serverSecret = crypto.HkdfExpandLabel(gocrypto.SHA256, initialSecret, "server in", gocrypto.SHA256.Size())
+	return
+}
+
+func computeInitialKeyAndIV(secret []byte) (key, iv []byte) {
+	key = crypto.HkdfExpandLabel(gocrypto.SHA256, secret, "key", 16)
+	iv = crypto.HkdfExpandLabel(gocrypto.SHA256, secret, "iv", 12)
+	return
+}

internal/handshake/initial_aead_test.go
@@ -1,12 +1,13 @@
-package crypto
+package handshake
 
 import (
 	"github.com/lucas-clemente/quic-go/internal/protocol"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("NullAEAD using AES-GCM", func() {
+var _ = Describe("Initial AEAD using AES-GCM", func() {
 	// values taken from https://github.com/quicwg/base-drafts/wiki/Test-Vector-for-the-Clear-Text-AEAD-key-derivation
 	Context("using the test vector from the QUIC WG Wiki", func() {
 		connID := protocol.ConnectionID([]byte{0x83, 0x94, 0xc8, 0xf0, 0x3e, 0x51, 0x57, 0x08})
@@ -29,7 +30,7 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 		It("computes the client key and IV", func() {
 			clientSecret, _ := computeSecrets(connID)
-			key, iv := computeNullAEADKeyAndIV(clientSecret)
+			key, iv := computeInitialKeyAndIV(clientSecret)
 			Expect(key).To(Equal([]byte{
 				0xf2, 0x92, 0x8f, 0x26, 0x14, 0xad, 0x6c, 0x20,
 				0xb9, 0xbd, 0x00, 0x8e, 0x9c, 0x89, 0x63, 0x1c,
@@ -42,7 +43,7 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 		It("computes the server key and IV", func() {
 			_, serverSecret := computeSecrets(connID)
-			key, iv := computeNullAEADKeyAndIV(serverSecret)
+			key, iv := computeInitialKeyAndIV(serverSecret)
 			Expect(key).To(Equal([]byte{
 				0xf5, 0x68, 0x17, 0xd0, 0xfc, 0x59, 0x5c, 0xfc,
 				0x0a, 0x2b, 0x0b, 0xcf, 0xb1, 0x87, 0x35, 0xec,
@@ -56,17 +57,17 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 
 	It("seals and opens", func() {
 		connectionID := protocol.ConnectionID([]byte{0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef})
-		clientAEAD, err := NewNullAEAD(connectionID, protocol.PerspectiveClient)
+		clientSealer, clientOpener, err := newInitialAEAD(connectionID, protocol.PerspectiveClient)
 		Expect(err).ToNot(HaveOccurred())
-		serverAEAD, err := NewNullAEAD(connectionID, protocol.PerspectiveServer)
+		serverSealer, serverOpener, err := newInitialAEAD(connectionID, protocol.PerspectiveServer)
 		Expect(err).ToNot(HaveOccurred())
 
-		clientMessage := clientAEAD.Seal(nil, []byte("foobar"), 42, []byte("aad"))
-		m, err := serverAEAD.Open(nil, clientMessage, 42, []byte("aad"))
+		clientMessage := clientSealer.Seal(nil, []byte("foobar"), 42, []byte("aad"))
+		m, err := serverOpener.Open(nil, clientMessage, 42, []byte("aad"))
 		Expect(err).ToNot(HaveOccurred())
 		Expect(m).To(Equal([]byte("foobar")))
-		serverMessage := serverAEAD.Seal(nil, []byte("raboof"), 99, []byte("daa"))
-		m, err = clientAEAD.Open(nil, serverMessage, 99, []byte("daa"))
+		serverMessage := serverSealer.Seal(nil, []byte("raboof"), 99, []byte("daa"))
+		m, err = clientOpener.Open(nil, serverMessage, 99, []byte("daa"))
 		Expect(err).ToNot(HaveOccurred())
 		Expect(m).To(Equal([]byte("raboof")))
 	})
@@ -74,13 +75,13 @@ var _ = Describe("NullAEAD using AES-GCM", func() {
 	It("doesn't work if initialized with different connection IDs", func() {
 		c1 := protocol.ConnectionID([]byte{0, 0, 0, 0, 0, 0, 0, 1})
 		c2 := protocol.ConnectionID([]byte{0, 0, 0, 0, 0, 0, 0, 2})
-		clientAEAD, err := NewNullAEAD(c1, protocol.PerspectiveClient)
+		clientSealer, _, err := newInitialAEAD(c1, protocol.PerspectiveClient)
 		Expect(err).ToNot(HaveOccurred())
-		serverAEAD, err := NewNullAEAD(c2, protocol.PerspectiveServer)
+		_, serverOpener, err := newInitialAEAD(c2, protocol.PerspectiveServer)
 		Expect(err).ToNot(HaveOccurred())
 
-		clientMessage := clientAEAD.Seal(nil, []byte("foobar"), 42, []byte("aad"))
-		_, err = serverAEAD.Open(nil, clientMessage, 42, []byte("aad"))
+		clientMessage := clientSealer.Seal(nil, []byte("foobar"), 42, []byte("aad"))
+		_, err = serverOpener.Open(nil, clientMessage, 42, []byte("aad"))
 		Expect(err).To(MatchError("cipher: message authentication failed"))
 	})
 })