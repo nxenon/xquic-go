starshipyard/quic-go

move the TransportState struct to the quictrace package
Prevents an import loop when passing the tracer to the ackhandler.

internal/ackhandler/interfaces.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 // SentPacketHandler handles ACKs received for outgoing packets
@@ -40,7 +41,7 @@ type SentPacketHandler interface {
 	OnAlarm() error
 
 	// report some congestion statistics. For tracing only.
-	GetCurrentState() *State
+	GetCurrentState() *quictrace.TransportState
 }
 
 // ReceivedPacketHandler handles ACKs needed to send for incoming packets
@@ -50,13 +51,4 @@ type ReceivedPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	GetAckFrame(protocol.EncryptionLevel) *wire.AckFrame
-}
-
-type State struct {
-	MinRTT      time.Duration
-	SmoothedRTT time.Duration
-	LatestRTT   time.Duration
-
-	BytesInFlight    protocol.ByteCount
-	CongestionWindow protocol.ByteCount
 }

internal/ackhandler/sent_packet_handler.go
@@ -11,6 +11,7 @@ import (
 	"github.com/lucas-clemente/quic-go/internal/qerr"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 const (
@@ -645,8 +646,8 @@ func (h *sentPacketHandler) ResetForRetry() error {
 	return nil
 }
 
-func (h *sentPacketHandler) GetCurrentState() *State {
-	return &State{
+func (h *sentPacketHandler) GetCurrentState() *quictrace.TransportState {
+	return &quictrace.TransportState{
 		MinRTT:           h.rttStats.MinRTT(),
 		SmoothedRTT:      h.rttStats.SmoothedOrInitialRTT(),
 		LatestRTT:        h.rttStats.LatestRTT(),

internal/mocks/ackhandler/sent_packet_handler.go
@@ -12,6 +12,7 @@ import (
 	ackhandler "github.com/lucas-clemente/quic-go/internal/ackhandler"
 	protocol "github.com/lucas-clemente/quic-go/internal/protocol"
 	wire "github.com/lucas-clemente/quic-go/internal/wire"
+	quictrace "github.com/lucas-clemente/quic-go/quictrace"
 )
 
 // MockSentPacketHandler is a mock of SentPacketHandler interface
@@ -81,10 +82,10 @@ func (mr *MockSentPacketHandlerMockRecorder) GetAlarmTimeout() *gomock.Call {
 }
 
 // GetCurrentState mocks base method
-func (m *MockSentPacketHandler) GetCurrentState() *ackhandler.State {
+func (m *MockSentPacketHandler) GetCurrentState() *quictrace.TransportState {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetCurrentState")
-	ret0, _ := ret[0].(*ackhandler.State)
+	ret0, _ := ret[0].(*quictrace.TransportState)
 	return ret0
 }
 

nxenon/xquic-go

internal/ackhandler/interfaces.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 // SentPacketHandler handles ACKs received for outgoing packets
@@ -41,7 +42,7 @@ type SentPacketHandler interface {
 	OnAlarm() error
 
 	// report some congestion statistics. For tracing only.
-	GetStats() *State
+	GetStats() *quictrace.TransportState
 }
 
 // ReceivedPacketHandler handles ACKs needed to send for incoming packets
@@ -52,13 +53,4 @@ type ReceivedPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	GetAckFrame(protocol.EncryptionLevel) *wire.AckFrame
-}
-
-type State struct {
-	MinRTT      time.Duration
-	SmoothedRTT time.Duration
-	LatestRTT   time.Duration
-
-	BytesInFlight    protocol.ByteCount
-	CongestionWindow protocol.ByteCount
 }

internal/ackhandler/sent_packet_handler.go
@@ -11,6 +11,7 @@ import (
 	"github.com/lucas-clemente/quic-go/internal/qerr"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 const (
@@ -671,8 +672,8 @@ func (h *sentPacketHandler) ResetForRetry() error {
 	return nil
 }
 
-func (h *sentPacketHandler) GetStats() *State {
-	return &State{
+func (h *sentPacketHandler) GetStats() *quictrace.TransportState {
+	return &quictrace.TransportState{
 		MinRTT:           h.rttStats.MinRTT(),
 		SmoothedRTT:      h.rttStats.SmoothedOrInitialRTT(),
 		LatestRTT:        h.rttStats.LatestRTT(),

internal/mocks/ackhandler/sent_packet_handler.go
@@ -12,6 +12,7 @@ import (
 	ackhandler "github.com/lucas-clemente/quic-go/internal/ackhandler"
 	protocol "github.com/lucas-clemente/quic-go/internal/protocol"
 	wire "github.com/lucas-clemente/quic-go/internal/wire"
+	quictrace "github.com/lucas-clemente/quic-go/quictrace"
 )
 
 // MockSentPacketHandler is a mock of SentPacketHandler interface
@@ -107,10 +108,10 @@ func (mr *MockSentPacketHandlerMockRecorder) GetLowestPacketNotConfirmedAcked() 
 }
 
 // GetStats mocks base method
-func (m *MockSentPacketHandler) GetStats() *ackhandler.State {
+func (m *MockSentPacketHandler) GetStats() *quictrace.TransportState {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetStats")
-	ret0, _ := ret[0].(*ackhandler.State)
+	ret0, _ := ret[0].(*quictrace.TransportState)
 	return ret0
 }
 