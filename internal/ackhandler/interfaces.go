starshipyard/quic-go

add tracing for sent and received packets

internal/ackhandler/interfaces.go
@@ -38,6 +38,9 @@ type SentPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	OnAlarm() error
+
+	// report some congestion statistics. For tracing only.
+	GetCurrentState() *State
 }
 
 // ReceivedPacketHandler handles ACKs needed to send for incoming packets
@@ -47,4 +50,13 @@ type ReceivedPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	GetAckFrame(protocol.EncryptionLevel) *wire.AckFrame
+}
+
+type State struct {
+	MinRTT      time.Duration
+	SmoothedRTT time.Duration
+	LatestRTT   time.Duration
+
+	BytesInFlight    protocol.ByteCount
+	CongestionWindow protocol.ByteCount
 }

internal/ackhandler/sent_packet_handler.go
@@ -644,3 +644,13 @@ func (h *sentPacketHandler) ResetForRetry() error {
 	h.updateLossDetectionAlarm()
 	return nil
 }
+
+func (h *sentPacketHandler) GetCurrentState() *State {
+	return &State{
+		MinRTT:           h.rttStats.MinRTT(),
+		SmoothedRTT:      h.rttStats.SmoothedOrInitialRTT(),
+		LatestRTT:        h.rttStats.LatestRTT(),
+		BytesInFlight:    h.bytesInFlight,
+		CongestionWindow: h.congestion.GetCongestionWindow(),
+	}
+}

quictrace/interface.go
@@ -1,0 +1,39 @@
+package quictrace
+
+import (
+	"time"
+
+	"github.com/lucas-clemente/quic-go/internal/ackhandler"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+)
+
+// A Tracer traces a QUIC connection
+type Tracer interface {
+	Trace(protocol.ConnectionID, Event)
+	EmitAll() map[string][]byte
+}
+
+// EventType is the type of an event
+type EventType uint8
+
+const (
+	// PacketSent means that a packet was sent
+	PacketSent EventType = 1 + iota
+	// PacketReceived means that a packet was received
+	PacketReceived
+	// PacketLost means that a packet was lost
+	PacketLost
+)
+
+// Event is a quic-traceable event
+type Event struct {
+	Time      time.Time
+	EventType EventType
+
+	TransportState  *ackhandler.State
+	EncryptionLevel protocol.EncryptionLevel
+	PacketNumber    protocol.PacketNumber
+	PacketSize      protocol.ByteCount
+	Frames          []wire.Frame
+}

quictrace/tracer.go
@@ -1,0 +1,188 @@
+package quictrace
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/lucas-clemente/quic-go/internal/ackhandler"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace/pb"
+)
+
+type traceEvent struct {
+	connID protocol.ConnectionID
+	ev     Event
+}
+
+// A tracer is used to trace a QUIC connection
+type tracer struct {
+	eventQueue chan traceEvent
+
+	events map[string] /* conn ID */ []traceEvent
+}
+
+var _ Tracer = &tracer{}
+
+// NewTracer creates a new Tracer
+func NewTracer() Tracer {
+	qt := &tracer{
+		eventQueue: make(chan traceEvent, 1<<10),
+		events:     make(map[string][]traceEvent),
+	}
+	go qt.run()
+	return qt
+}
+
+// Trace traces an event
+func (t *tracer) Trace(connID protocol.ConnectionID, ev Event) {
+	t.eventQueue <- traceEvent{connID: connID, ev: ev}
+}
+
+func (t *tracer) run() {
+	for tev := range t.eventQueue {
+		key := string(tev.connID)
+		if _, ok := t.events[key]; !ok {
+			t.events[key] = make([]traceEvent, 0, 10*1<<10)
+		}
+		t.events[key] = append(t.events[key], tev)
+	}
+}
+
+func (t *tracer) EmitAll() map[string][]byte {
+	traces := make(map[string][]byte)
+	for connID := range t.events {
+		data, err := t.emitByConnIDAsString(connID)
+		if err != nil {
+			panic(err)
+		}
+		traces[connID] = data
+	}
+	return traces
+}
+
+// Emit emits the serialized protobuf that will be consumed by quic-trace
+func (t *tracer) Emit(connID protocol.ConnectionID) ([]byte, error) {
+	return t.emitByConnIDAsString(string(connID))
+}
+
+func (t *tracer) emitByConnIDAsString(connID string) ([]byte, error) {
+	events, ok := t.events[connID]
+	if !ok {
+		return nil, fmt.Errorf("No trace found for connection ID %s", connID)
+	}
+	trace := &pb.Trace{
+		DestinationConnectionId: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		SourceConnectionId:      []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		ProtocolVersion:         []byte{0xff, 0, 0, 19},
+		Events:                  make([]*pb.Event, len(events)),
+	}
+	var startTime time.Time
+	for i, ev := range events {
+		event := ev.ev
+		if i == 0 {
+			startTime = event.Time
+		}
+
+		packetNumber := uint64(event.PacketNumber)
+		packetSize := uint64(event.PacketSize)
+
+		trace.Events[i] = &pb.Event{
+			TimeUs:          durationToUs(event.Time.Sub(startTime)),
+			EventType:       getEventType(event.EventType),
+			PacketSize:      &packetSize,
+			PacketNumber:    &packetNumber,
+			TransportState:  getTransportState(event.TransportState),
+			EncryptionLevel: getEncryptionLevel(event.EncryptionLevel),
+			Frames:          getFrames(event.Frames),
+		}
+	}
+	delete(t.events, connID)
+	return proto.Marshal(trace)
+}
+
+func getEventType(evType EventType) *pb.EventType {
+	var t pb.EventType
+	switch evType {
+	case PacketSent:
+		t = pb.EventType_PACKET_SENT
+	case PacketReceived:
+		t = pb.EventType_PACKET_RECEIVED
+	case PacketLost:
+		t = pb.EventType_PACKET_LOST
+	default:
+		panic("unknown event type")
+	}
+	return &t
+}
+
+func getEncryptionLevel(encLevel protocol.EncryptionLevel) *pb.EncryptionLevel {
+	enc := pb.EncryptionLevel_ENCRYPTION_UNKNOWN
+	switch encLevel {
+	case protocol.EncryptionInitial:
+		enc = pb.EncryptionLevel_ENCRYPTION_INITIAL
+	case protocol.EncryptionHandshake:
+		enc = pb.EncryptionLevel_ENCRYPTION_HANDSHAKE
+	case protocol.Encryption1RTT:
+		enc = pb.EncryptionLevel_ENCRYPTION_1RTT
+	}
+	return &enc
+}
+
+func getFrames(wframes []wire.Frame) []*pb.Frame {
+	streamFrameType := pb.FrameType_STREAM
+	ackFrameType := pb.FrameType_ACK
+	var frames []*pb.Frame
+	for _, frame := range wframes {
+		switch f := frame.(type) {
+		case *wire.StreamFrame:
+			streamID := uint64(f.StreamID)
+			offset := uint64(f.Offset)
+			length := uint64(f.DataLen())
+			frames = append(frames, &pb.Frame{
+				FrameType: &streamFrameType,
+				StreamFrameInfo: &pb.StreamFrameInfo{
+					StreamId: &streamID,
+					Offset:   &offset,
+					Length:   &length,
+				},
+			})
+		case *wire.AckFrame:
+			var ackedPackets []*pb.AckBlock
+			for _, ackBlock := range f.AckRanges {
+				firstPacket := uint64(ackBlock.Smallest)
+				lastPacket := uint64(ackBlock.Largest)
+				ackedPackets = append(ackedPackets, &pb.AckBlock{
+					FirstPacket: &firstPacket,
+					LastPacket:  &lastPacket,
+				})
+			}
+			frames = append(frames, &pb.Frame{
+				FrameType: &ackFrameType,
+				AckInfo: &pb.AckInfo{
+					AckDelayUs:   durationToUs(f.DelayTime),
+					AckedPackets: ackedPackets,
+				},
+			})
+		}
+	}
+	return frames
+}
+
+func getTransportState(state *ackhandler.State) *pb.TransportState {
+	bytesInFlight := uint64(state.BytesInFlight)
+	congestionWindow := uint64(state.CongestionWindow)
+	return &pb.TransportState{
+		MinRttUs:      durationToUs(state.MinRTT),
+		SmoothedRttUs: durationToUs(state.SmoothedRTT),
+		LastRttUs:     durationToUs(state.LatestRTT),
+		InFlightBytes: &bytesInFlight,
+		CwndBytes:     &congestionWindow,
+	}
+}
+
+func durationToUs(d time.Duration) *uint64 {
+	dur := uint64(d / 1000)
+	return &dur
+}

session.go
@@ -20,6 +20,7 @@ import (
 	"github.com/lucas-clemente/quic-go/internal/qerr"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 type unpacker interface {
@@ -650,6 +651,10 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 		}
 	}
 
+	// Only used for tracing.
+	// If we're not tracing, this slice will always remain empty.
+	var frames []wire.Frame
+
 	r := bytes.NewReader(packet.data)
 	var isRetransmittable bool
 	for {
@@ -663,9 +668,24 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 		if ackhandler.IsFrameRetransmittable(frame) {
 			isRetransmittable = true
 		}
+		if s.config.QuicTracer != nil {
+			frames = append(frames, frame)
+		}
 		if err := s.handleFrame(frame, packet.packetNumber, packet.encryptionLevel); err != nil {
 			return err
 		}
+	}
+
+	if s.config.QuicTracer != nil {
+		s.config.QuicTracer.Trace(s.origDestConnID, quictrace.Event{
+			Time:            time.Now(),
+			EventType:       quictrace.PacketReceived,
+			TransportState:  s.sentPacketHandler.GetCurrentState(),
+			EncryptionLevel: packet.encryptionLevel,
+			PacketNumber:    packet.packetNumber,
+			PacketSize:      protocol.ByteCount(len(packet.data)),
+			Frames:          frames,
+		})
 	}
 
 	if err := s.receivedPacketHandler.ReceivedPacket(packet.packetNumber, packet.encryptionLevel, rcvTime, isRetransmittable); err != nil {
@@ -1115,6 +1135,17 @@ func (s *session) sendPackedPacket(packet *packedPacket) error {
 	defer packet.buffer.Release()
 	if s.firstRetransmittablePacketAfterIdleSentTime.IsZero() && packet.IsRetransmittable() {
 		s.firstRetransmittablePacketAfterIdleSentTime = time.Now()
+	}
+	if s.config.QuicTracer != nil {
+		s.config.QuicTracer.Trace(s.origDestConnID, quictrace.Event{
+			Time:            time.Now(),
+			EventType:       quictrace.PacketSent,
+			TransportState:  s.sentPacketHandler.GetCurrentState(),
+			EncryptionLevel: packet.EncryptionLevel(),
+			PacketNumber:    packet.header.PacketNumber,
+			PacketSize:      protocol.ByteCount(len(packet.raw)),
+			Frames:          packet.frames,
+		})
 	}
 	s.logPacket(packet)
 	return s.conn.Write(packet.raw)

nxenon/xquic-go

internal/ackhandler/interfaces.go
@@ -39,6 +39,9 @@ type SentPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	OnAlarm() error
+
+	// report some congestion statistics. For tracing only.
+	GetStats() *State
 }
 
 // ReceivedPacketHandler handles ACKs needed to send for incoming packets
@@ -49,4 +52,13 @@ type ReceivedPacketHandler interface {
 
 	GetAlarmTimeout() time.Time
 	GetAckFrame(protocol.EncryptionLevel) *wire.AckFrame
+}
+
+type State struct {
+	MinRTT      time.Duration
+	SmoothedRTT time.Duration
+	LatestRTT   time.Duration
+
+	BytesInFlight    protocol.ByteCount
+	CongestionWindow protocol.ByteCount
 }

internal/ackhandler/sent_packet_handler.go
@@ -670,3 +670,13 @@ func (h *sentPacketHandler) ResetForRetry() error {
 	h.updateLossDetectionAlarm()
 	return nil
 }
+
+func (h *sentPacketHandler) GetStats() *State {
+	return &State{
+		MinRTT:           h.rttStats.MinRTT(),
+		SmoothedRTT:      h.rttStats.SmoothedOrInitialRTT(),
+		LatestRTT:        h.rttStats.LatestRTT(),
+		BytesInFlight:    h.bytesInFlight,
+		CongestionWindow: h.congestion.GetCongestionWindow(),
+	}
+}

quictrace/interface.go
@@ -1,0 +1,39 @@
+package quictrace
+
+import (
+	"time"
+
+	"github.com/lucas-clemente/quic-go/internal/ackhandler"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+)
+
+// A Tracer traces a QUIC connection
+type Tracer interface {
+	Trace(protocol.ConnectionID, Event)
+	GetAllTraces() map[string][]byte
+}
+
+// EventType is the type of an event
+type EventType uint8
+
+const (
+	// PacketSent means that a packet was sent
+	PacketSent EventType = 1 + iota
+	// PacketReceived means that a packet was received
+	PacketReceived
+	// PacketLost means that a packet was lost
+	PacketLost
+)
+
+// Event is a quic-traceable event
+type Event struct {
+	Time      time.Time
+	EventType EventType
+
+	TransportState  *ackhandler.State
+	EncryptionLevel protocol.EncryptionLevel
+	PacketNumber    protocol.PacketNumber
+	PacketSize      protocol.ByteCount
+	Frames          []wire.Frame
+}

quictrace/tracer.go
@@ -1,0 +1,189 @@
+package quictrace
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/lucas-clemente/quic-go/internal/ackhandler"
+	"github.com/lucas-clemente/quic-go/internal/protocol"
+	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace/pb"
+)
+
+type traceEvent struct {
+	connID protocol.ConnectionID
+	ev     Event
+}
+
+// A tracer is used to trace a QUIC connection
+type tracer struct {
+	eventQueue chan traceEvent
+
+	events map[string] /* conn ID */ []traceEvent
+}
+
+var _ Tracer = &tracer{}
+
+// NewTracer creates a new Tracer
+func NewTracer() Tracer {
+	qt := &tracer{
+		eventQueue: make(chan traceEvent, 1<<10),
+		events:     make(map[string][]traceEvent),
+	}
+	go qt.run()
+	return qt
+}
+
+// Trace traces an event
+func (t *tracer) Trace(connID protocol.ConnectionID, ev Event) {
+	t.eventQueue <- traceEvent{connID: connID, ev: ev}
+}
+
+func (t *tracer) run() {
+	for tev := range t.eventQueue {
+		key := string(tev.connID)
+		if _, ok := t.events[key]; !ok {
+			t.events[key] = make([]traceEvent, 0, 10*1<<10)
+		}
+		t.events[key] = append(t.events[key], tev)
+	}
+}
+
+func (t *tracer) GetAllTraces() map[string][]byte {
+	traces := make(map[string][]byte)
+	for connID := range t.events {
+		data, err := t.emitByConnIDAsString(connID)
+		if err != nil {
+			panic(err)
+		}
+		traces[connID] = data
+	}
+	return traces
+}
+
+// Emit emits the serialized protobuf that will be consumed by quic-trace
+func (t *tracer) Emit(connID protocol.ConnectionID) ([]byte, error) {
+	return t.emitByConnIDAsString(string(connID))
+}
+
+func (t *tracer) emitByConnIDAsString(connID string) ([]byte, error) {
+	events, ok := t.events[connID]
+	if !ok {
+		return nil, fmt.Errorf("No trace found for connection ID %s", connID)
+	}
+	trace := &pb.Trace{
+		DestinationConnectionId: []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		SourceConnectionId:      []byte{1, 2, 3, 4, 5, 6, 7, 8},
+		ProtocolVersion:         []byte{0xff, 0, 0, 19},
+		Events:                  make([]*pb.Event, len(events)),
+	}
+	var startTime time.Time
+	for i, ev := range events {
+		event := ev.ev
+		if i == 0 {
+			startTime = event.Time
+		}
+
+		packetNumber := uint64(event.PacketNumber)
+		packetSize := uint64(event.PacketSize)
+
+		trace.Events[i] = &pb.Event{
+			TimeUs:          durationToUs(event.Time.Sub(startTime)),
+			EventType:       getEventType(event.EventType),
+			PacketSize:      &packetSize,
+			PacketNumber:    &packetNumber,
+			TransportState:  getTransportState(event.TransportState),
+			EncryptionLevel: getEncryptionLevel(event.EncryptionLevel),
+			Frames:          getFrames(event.Frames),
+		}
+	}
+	delete(t.events, connID)
+	return proto.Marshal(trace)
+}
+
+func getEventType(evType EventType) *pb.EventType {
+	var t pb.EventType
+	switch evType {
+	case PacketSent:
+		t = pb.EventType_PACKET_SENT
+	case PacketReceived:
+		t = pb.EventType_PACKET_RECEIVED
+	case PacketLost:
+		t = pb.EventType_PACKET_LOST
+	default:
+		panic("unknown event type")
+	}
+	return &t
+}
+
+func getEncryptionLevel(encLevel protocol.EncryptionLevel) *pb.EncryptionLevel {
+	enc := pb.EncryptionLevel_ENCRYPTION_UNKNOWN
+	switch encLevel {
+	case protocol.EncryptionInitial:
+		enc = pb.EncryptionLevel_ENCRYPTION_INITIAL
+	case protocol.EncryptionHandshake:
+		enc = pb.EncryptionLevel_ENCRYPTION_HANDSHAKE
+	case protocol.Encryption1RTT:
+		enc = pb.EncryptionLevel_ENCRYPTION_1RTT
+	}
+	return &enc
+}
+
+func getFrames(wframes []wire.Frame) []*pb.Frame {
+	streamFrameType := pb.FrameType_STREAM
+	ackFrameType := pb.FrameType_ACK
+	var frames []*pb.Frame
+	for _, frame := range wframes {
+		switch f := frame.(type) {
+		case *wire.StreamFrame:
+			streamID := uint64(f.StreamID)
+			offset := uint64(f.Offset)
+			length := uint64(f.DataLen())
+			frames = append(frames, &pb.Frame{
+				FrameType: &streamFrameType,
+				StreamFrameInfo: &pb.StreamFrameInfo{
+					StreamId: &streamID,
+					Offset:   &offset,
+					Length:   &length,
+				},
+			})
+		case *wire.AckFrame:
+			var ackedPackets []*pb.AckBlock
+			for _, ackBlock := range f.AckRanges {
+				firstPacket := uint64(ackBlock.Smallest)
+				lastPacket := uint64(ackBlock.Largest)
+				ackedPackets = append(ackedPackets, &pb.AckBlock{
+					FirstPacket: &firstPacket,
+					LastPacket:  &lastPacket,
+				})
+			}
+			frames = append(frames, &pb.Frame{
+				FrameType: &ackFrameType,
+				AckInfo: &pb.AckInfo{
+					AckDelayUs:   durationToUs(f.DelayTime),
+					AckedPackets: ackedPackets,
+				},
+			})
+		}
+	}
+	return frames
+}
+
+func getTransportState(state *ackhandler.State) *pb.TransportState {
+	bytesInFlight := uint64(state.BytesInFlight)
+	congestionWindow := uint64(state.CongestionWindow)
+	return &pb.TransportState{
+		MinRttUs:      durationToUs(state.MinRTT),
+		SmoothedRttUs: durationToUs(state.SmoothedRTT),
+		LastRttUs:     durationToUs(state.LatestRTT),
+		InFlightBytes: &bytesInFlight,
+		CwndBytes:     &congestionWindow,
+	}
+}
+
+func durationToUs(d time.Duration) *uint64 {
+	dur := uint64(d / 1000)
+	return &dur
+}

session.go
@@ -20,6 +20,7 @@ import (
 	"github.com/lucas-clemente/quic-go/internal/qerr"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"github.com/lucas-clemente/quic-go/quictrace"
 )
 
 type unpacker interface {
@@ -665,6 +666,10 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 	s.firstAckElicitingPacketAfterIdleSentTime = time.Time{}
 	s.keepAlivePingSent = false
 
+	// Only used for tracing.
+	// If we're not tracing, this slice will always remain empty.
+	var frames []wire.Frame
+
 	r := bytes.NewReader(packet.data)
 	var isAckEliciting bool
 	for {
@@ -678,9 +683,24 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 		if ackhandler.IsFrameAckEliciting(frame) {
 			isAckEliciting = true
 		}
+		if s.config.QuicTracer != nil {
+			frames = append(frames, frame)
+		}
 		if err := s.handleFrame(frame, packet.packetNumber, packet.encryptionLevel); err != nil {
 			return err
 		}
+	}
+
+	if s.config.QuicTracer != nil {
+		s.config.QuicTracer.Trace(s.origDestConnID, quictrace.Event{
+			Time:            time.Now(),
+			EventType:       quictrace.PacketReceived,
+			TransportState:  s.sentPacketHandler.GetStats(),
+			EncryptionLevel: packet.encryptionLevel,
+			PacketNumber:    packet.packetNumber,
+			PacketSize:      protocol.ByteCount(len(packet.data)),
+			Frames:          frames,
+		})
 	}
 
 	if err := s.receivedPacketHandler.ReceivedPacket(packet.packetNumber, packet.encryptionLevel, rcvTime, isAckEliciting); err != nil {
@@ -1152,6 +1172,17 @@ func (s *session) sendPackedPacket(packet *packedPacket) error {
 	defer packet.buffer.Release()
 	if s.firstAckElicitingPacketAfterIdleSentTime.IsZero() && packet.IsAckEliciting() {
 		s.firstAckElicitingPacketAfterIdleSentTime = time.Now()
+	}
+	if s.config.QuicTracer != nil {
+		s.config.QuicTracer.Trace(s.origDestConnID, quictrace.Event{
+			Time:            time.Now(),
+			EventType:       quictrace.PacketSent,
+			TransportState:  s.sentPacketHandler.GetStats(),
+			EncryptionLevel: packet.EncryptionLevel(),
+			PacketNumber:    packet.header.PacketNumber,
+			PacketSize:      protocol.ByteCount(len(packet.raw)),
+			Frames:          packet.frames,
+		})
 	}
 	s.logPacket(packet)
 	return s.conn.Write(packet.raw)