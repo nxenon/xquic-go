theDEADpool/quic-go

record the congestion state for tracing before processing the ACK frame

session.go
@@ -661,6 +661,10 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 	// Only used for tracing.
 	// If we're not tracing, this slice will always remain empty.
 	var frames []wire.Frame
+	var transportState *quictrace.TransportState
+	if s.traceCallback != nil {
+		transportState = s.sentPacketHandler.GetCurrentState()
+	}
 
 	r := bytes.NewReader(packet.data)
 	var isAckEliciting bool
@@ -687,7 +691,7 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 		s.traceCallback(quictrace.Event{
 			Time:            time.Now(),
 			EventType:       quictrace.PacketReceived,
-			TransportState:  s.sentPacketHandler.GetCurrentState(),
+			TransportState:  transportState,
 			EncryptionLevel: packet.encryptionLevel,
 			PacketNumber:    packet.packetNumber,
 			PacketSize:      protocol.ByteCount(len(packet.data)),

nxenon/xquic-go

session.go
@@ -676,6 +676,10 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 	// Only used for tracing.
 	// If we're not tracing, this slice will always remain empty.
 	var frames []wire.Frame
+	var transportState *quictrace.TransportState
+	if s.traceCallback != nil {
+		transportState = s.sentPacketHandler.GetStats()
+	}
 
 	r := bytes.NewReader(packet.data)
 	var isAckEliciting bool
@@ -702,7 +706,7 @@ func (s *session) handleUnpackedPacket(packet *unpackedPacket, rcvTime time.Time
 		s.traceCallback(quictrace.Event{
 			Time:            time.Now(),
 			EventType:       quictrace.PacketReceived,
-			TransportState:  s.sentPacketHandler.GetStats(),
+			TransportState:  transportState,
 			EncryptionLevel: packet.encryptionLevel,
 			PacketNumber:    packet.packetNumber,
 			PacketSize:      protocol.ByteCount(len(packet.data)),