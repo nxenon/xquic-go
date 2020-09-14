fcdtc/quic-go

add the AEAD_LIMIT_REACHED error code

internal/qerr/error_codes.go
@@ -25,6 +25,7 @@ const (
 	InvalidToken            ErrorCode = 0xb
 	ApplicationError        ErrorCode = 0xc
 	CryptoBufferExceeded    ErrorCode = 0xd
+	AEADLimitReached        ErrorCode = 0xe
 )
 
 func (e ErrorCode) isCryptoError() bool {
@@ -77,6 +78,8 @@ func (e ErrorCode) String() string {
 		return "APPLICATION_ERROR"
 	case CryptoBufferExceeded:
 		return "CRYPTO_BUFFER_EXCEEDED"
+	case AEADLimitReached:
+		return "AEAD_LIMIT_REACHED"
 	default:
 		if e.isCryptoError() {
 			return "CRYPTO_ERROR"

nxenon/xquic-go

internal/qerr/error_codes.go
@@ -26,6 +26,7 @@ const (
 	ApplicationError        ErrorCode = 0xc
 	CryptoBufferExceeded    ErrorCode = 0xd
 	KeyUpdateError          ErrorCode = 0xe
+	AEADLimitReached        ErrorCode = 0xf
 )
 
 func (e ErrorCode) isCryptoError() bool {
@@ -80,6 +81,8 @@ func (e ErrorCode) String() string {
 		return "CRYPTO_BUFFER_EXCEEDED"
 	case KeyUpdateError:
 		return "KEY_UPDATE_ERROR"
+	case AEADLimitReached:
+		return "AEAD_LIMIT_REACHED"
 	default:
 		if e.isCryptoError() {
 			return "CRYPTO_ERROR"