mengelbart/quic-go

increase UDP receive buffer size

packet_handler_map.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"hash"
+	"log"
 	"net"
 	"sync"
 	"time"
@@ -53,6 +54,40 @@ type packetHandlerMap struct {
 
 var _ packetHandlerManager = &packetHandlerMap{}
 
+func setReceiveBuffer(c net.PacketConn, logger utils.Logger) {
+	conn, ok := c.(interface{ SetReadBuffer(int) error })
+	if !ok {
+		logger.Debugf("Connection doesn't allow setting of receive buffer size")
+		return
+	}
+	size, err := inspectReadBuffer(c)
+	if err != nil {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if size >= protocol.DesiredReceiveBufferSize {
+		logger.Debugf("Conn has receive buffer of %d kB (wanted: at least %d kB)", size/1024, protocol.DesiredReceiveBufferSize/1024)
+	}
+	if err := conn.SetReadBuffer(protocol.DesiredReceiveBufferSize); err != nil {
+		log.Printf("Failed to increase receive buffer size: %s\n", err)
+		return
+	}
+	newSize, err := inspectReadBuffer(c)
+	if err != nil {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if newSize == size {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if newSize < protocol.DesiredReceiveBufferSize {
+		log.Printf("Failed to sufficiently increase receive buffer size. Was: %d kb, wanted: %d kb, got: %d kb.", size/1024, protocol.DesiredReceiveBufferSize/1024, newSize/1024)
+		return
+	}
+	logger.Debugf("Increased receive buffer size to %d kb", newSize/1024)
+}
+
 func newPacketHandlerMap(
 	c net.PacketConn,
 	connIDLen int,
@@ -60,6 +95,7 @@ func newPacketHandlerMap(
 	tracer logging.Tracer,
 	logger utils.Logger,
 ) (packetHandlerManager, error) {
+	setReceiveBuffer(c, logger)
 	conn, err := wrapConn(c)
 	if err != nil {
 		return nil, err
@@ -81,7 +117,6 @@ func newPacketHandlerMap(
 	if logger.Debug() {
 		go m.logUsage()
 	}
-
 	return m, nil
 }
 

nxenon/xquic-go

packet_handler_map.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"hash"
+	"log"
 	"net"
 	"sync"
 	"time"
@@ -53,6 +54,40 @@ type packetHandlerMap struct {
 
 var _ packetHandlerManager = &packetHandlerMap{}
 
+func setReceiveBuffer(c net.PacketConn, logger utils.Logger) {
+	conn, ok := c.(interface{ SetReadBuffer(int) error })
+	if !ok {
+		logger.Debugf("Connection doesn't allow setting of receive buffer size")
+		return
+	}
+	size, err := inspectReadBuffer(c)
+	if err != nil {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if size >= protocol.DesiredReceiveBufferSize {
+		logger.Debugf("Conn has receive buffer of %d kiB (wanted: at least %d kiB)", size/1024, protocol.DesiredReceiveBufferSize/1024)
+	}
+	if err := conn.SetReadBuffer(protocol.DesiredReceiveBufferSize); err != nil {
+		log.Printf("Failed to increase receive buffer size: %s\n", err)
+		return
+	}
+	newSize, err := inspectReadBuffer(c)
+	if err != nil {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if newSize == size {
+		log.Printf("Failed to determine receive buffer size: %s", err)
+		return
+	}
+	if newSize < protocol.DesiredReceiveBufferSize {
+		log.Printf("Failed to sufficiently increase receive buffer size. Was: %d kiB, wanted: %d kiB, got: %d kiB.", size/1024, protocol.DesiredReceiveBufferSize/1024, newSize/1024)
+		return
+	}
+	logger.Debugf("Increased receive buffer size to %d kiB", newSize/1024)
+}
+
 func newPacketHandlerMap(
 	c net.PacketConn,
 	connIDLen int,
@@ -60,6 +95,7 @@ func newPacketHandlerMap(
 	tracer logging.Tracer,
 	logger utils.Logger,
 ) (packetHandlerManager, error) {
+	setReceiveBuffer(c, logger)
 	conn, err := wrapConn(c)
 	if err != nil {
 		return nil, err
@@ -81,7 +117,6 @@ func newPacketHandlerMap(
 	if logger.Debug() {
 		go m.logUsage()
 	}
-
 	return m, nil
 }
 