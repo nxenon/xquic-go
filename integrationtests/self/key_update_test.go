quic-go/quic-go

add a generic Log() function to the connection tracer

integrationtests/self/key_update_test.go
@@ -89,6 +89,7 @@ func (t *connTracer) DroppedKey(logging.KeyPhase)                               
 func (t *connTracer) SetLossTimer(logging.TimerType, logging.EncryptionLevel, time.Time) {}
 func (t *connTracer) LossTimerExpired(logging.TimerType, logging.EncryptionLevel)        {}
 func (t *connTracer) LossTimerCanceled()                                                 {}
+func (t *connTracer) Log(string, string)                                                 {}
 func (t *connTracer) Close()                                                             {}
 
 var _ = Describe("Key Update tests", func() {

internal/mocks/logging/connection_tracer.go
@@ -111,6 +111,18 @@ func (mr *MockConnectionTracerMockRecorder) DroppedPacket(arg0, arg1, arg2 inter
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DroppedPacket", reflect.TypeOf((*MockConnectionTracer)(nil).DroppedPacket), arg0, arg1, arg2)
 }
 
+// Log mocks base method
+func (m *MockConnectionTracer) Log(arg0, arg1 string) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Log", arg0, arg1)
+}
+
+// Log indicates an expected call of Log
+func (mr *MockConnectionTracerMockRecorder) Log(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Log", reflect.TypeOf((*MockConnectionTracer)(nil).Log), arg0, arg1)
+}
+
 // LossTimerCanceled mocks base method
 func (m *MockConnectionTracer) LossTimerCanceled() {
 	m.ctrl.T.Helper()

logging/interface.go
@@ -124,6 +124,7 @@ type ConnectionTracer interface {
 	SetLossTimer(TimerType, EncryptionLevel, time.Time)
 	LossTimerExpired(TimerType, EncryptionLevel)
 	LossTimerCanceled()
+	Log(name, msg string)
 	// Close is called when the connection is closed.
 	Close()
 }

logging/mock_connection_tracer_test.go
@@ -110,6 +110,18 @@ func (mr *MockConnectionTracerMockRecorder) DroppedPacket(arg0, arg1, arg2 inter
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DroppedPacket", reflect.TypeOf((*MockConnectionTracer)(nil).DroppedPacket), arg0, arg1, arg2)
 }
 
+// Log mocks base method
+func (m *MockConnectionTracer) Log(arg0, arg1 string) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Log", arg0, arg1)
+}
+
+// Log indicates an expected call of Log
+func (mr *MockConnectionTracerMockRecorder) Log(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Log", reflect.TypeOf((*MockConnectionTracer)(nil).Log), arg0, arg1)
+}
+
 // LossTimerCanceled mocks base method
 func (m *MockConnectionTracer) LossTimerCanceled() {
 	m.ctrl.T.Helper()

logging/multiplex.go
@@ -186,6 +186,12 @@ func (m *connTracerMultiplexer) LossTimerCanceled() {
 	}
 }
 
+func (m *connTracerMultiplexer) Log(name, msg string) {
+	for _, t := range m.tracers {
+		t.Log(name, msg)
+	}
+}
+
 func (m *connTracerMultiplexer) Close() {
 	for _, t := range m.tracers {
 		t.Close()

metrics/metrics.go
@@ -218,4 +218,5 @@ func (t *connTracer) DroppedKey(logging.KeyPhase)                               
 func (t *connTracer) SetLossTimer(logging.TimerType, logging.EncryptionLevel, time.Time) {}
 func (t *connTracer) LossTimerExpired(logging.TimerType, logging.EncryptionLevel)        {}
 func (t *connTracer) LossTimerCanceled()                                                 {}
+func (t *connTracer) Log(string, string)                                                 {}
 func (t *connTracer) Close()                                                             {}

qlog/qlog.go
@@ -417,3 +417,12 @@ func (t *connectionTracer) LossTimerCanceled() {
 	t.recordEvent(time.Now(), &eventLossTimerCanceled{})
 	t.mutex.Unlock()
 }
+
+func (t *connectionTracer) Log(name, msg string) {
+	t.mutex.Lock()
+	t.recordEvent(time.Now(), &eventGeneric{
+		name: name,
+		msg:  msg,
+	})
+	t.mutex.Unlock()
+}

qlog/qlog_test.go
@@ -698,6 +698,17 @@ var _ = Describe("Tracing", func() {
 				Expect(ev).To(HaveLen(1))
 				Expect(ev).To(HaveKeyWithValue("event_type", "cancelled"))
 			})
+
+			It("records a generic event", func() {
+				tracer.Log("foo", "bar")
+				entry := exportAndParseSingle()
+				Expect(entry.Time).To(BeTemporally("~", time.Now(), scaleDuration(10*time.Millisecond)))
+				Expect(entry.Category).To(Equal("transport"))
+				Expect(entry.Name).To(Equal("foo"))
+				ev := entry.Event
+				Expect(ev).To(HaveLen(1))
+				Expect(ev).To(HaveKeyWithValue("details", "bar"))
+			})
 		})
 	})
 })

nxenon/xquic-go

integrationtests/self/key_update_test.go
@@ -89,6 +89,7 @@ func (t *connTracer) DroppedKey(logging.KeyPhase)                               
 func (t *connTracer) SetLossTimer(logging.TimerType, logging.EncryptionLevel, time.Time) {}
 func (t *connTracer) LossTimerExpired(logging.TimerType, logging.EncryptionLevel)        {}
 func (t *connTracer) LossTimerCanceled()                                                 {}
+func (t *connTracer) Debug(string, string)                                               {}
 func (t *connTracer) Close()                                                             {}
 
 var _ = Describe("Key Update tests", func() {

internal/mocks/logging/connection_tracer.go
@@ -75,6 +75,18 @@ func (mr *MockConnectionTracerMockRecorder) ClosedConnection(arg0 interface{}) *
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ClosedConnection", reflect.TypeOf((*MockConnectionTracer)(nil).ClosedConnection), arg0)
 }
 
+// Debug mocks base method
+func (m *MockConnectionTracer) Debug(arg0, arg1 string) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Debug", arg0, arg1)
+}
+
+// Debug indicates an expected call of Debug
+func (mr *MockConnectionTracerMockRecorder) Debug(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Debug", reflect.TypeOf((*MockConnectionTracer)(nil).Debug), arg0, arg1)
+}
+
 // DroppedEncryptionLevel mocks base method
 func (m *MockConnectionTracer) DroppedEncryptionLevel(arg0 protocol.EncryptionLevel) {
 	m.ctrl.T.Helper()

logging/interface.go
@@ -126,4 +126,5 @@ type ConnectionTracer interface {
 	LossTimerCanceled()
 	// Close is called when the connection is closed.
 	Close()
+	Debug(name, msg string)
 }

logging/mock_connection_tracer_test.go
@@ -74,6 +74,18 @@ func (mr *MockConnectionTracerMockRecorder) ClosedConnection(arg0 interface{}) *
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ClosedConnection", reflect.TypeOf((*MockConnectionTracer)(nil).ClosedConnection), arg0)
 }
 
+// Debug mocks base method
+func (m *MockConnectionTracer) Debug(arg0, arg1 string) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Debug", arg0, arg1)
+}
+
+// Debug indicates an expected call of Debug
+func (mr *MockConnectionTracerMockRecorder) Debug(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Debug", reflect.TypeOf((*MockConnectionTracer)(nil).Debug), arg0, arg1)
+}
+
 // DroppedEncryptionLevel mocks base method
 func (m *MockConnectionTracer) DroppedEncryptionLevel(arg0 protocol.EncryptionLevel) {
 	m.ctrl.T.Helper()

logging/multiplex.go
@@ -186,6 +186,12 @@ func (m *connTracerMultiplexer) LossTimerCanceled() {
 	}
 }
 
+func (m *connTracerMultiplexer) Debug(name, msg string) {
+	for _, t := range m.tracers {
+		t.Debug(name, msg)
+	}
+}
+
 func (m *connTracerMultiplexer) Close() {
 	for _, t := range m.tracers {
 		t.Close()

metrics/metrics.go
@@ -218,4 +218,5 @@ func (t *connTracer) DroppedKey(logging.KeyPhase)                               
 func (t *connTracer) SetLossTimer(logging.TimerType, logging.EncryptionLevel, time.Time) {}
 func (t *connTracer) LossTimerExpired(logging.TimerType, logging.EncryptionLevel)        {}
 func (t *connTracer) LossTimerCanceled()                                                 {}
+func (t *connTracer) Debug(string, string)                                               {}
 func (t *connTracer) Close()                                                             {}

qlog/qlog.go
@@ -417,3 +417,12 @@ func (t *connectionTracer) LossTimerCanceled() {
 	t.recordEvent(time.Now(), &eventLossTimerCanceled{})
 	t.mutex.Unlock()
 }
+
+func (t *connectionTracer) Debug(name, msg string) {
+	t.mutex.Lock()
+	t.recordEvent(time.Now(), &eventGeneric{
+		name: name,
+		msg:  msg,
+	})
+	t.mutex.Unlock()
+}

qlog/qlog_test.go
@@ -698,6 +698,17 @@ var _ = Describe("Tracing", func() {
 				Expect(ev).To(HaveLen(1))
 				Expect(ev).To(HaveKeyWithValue("event_type", "cancelled"))
 			})
+
+			It("records a generic event", func() {
+				tracer.Debug("foo", "bar")
+				entry := exportAndParseSingle()
+				Expect(entry.Time).To(BeTemporally("~", time.Now(), scaleDuration(10*time.Millisecond)))
+				Expect(entry.Category).To(Equal("transport"))
+				Expect(entry.Name).To(Equal("foo"))
+				ev := entry.Event
+				Expect(ev).To(HaveLen(1))
+				Expect(ev).To(HaveKeyWithValue("details", "bar"))
+			})
 		})
 	})
 })