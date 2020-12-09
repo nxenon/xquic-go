mator2006/quic-go

tell if a peer supports DATAGRAM frames in the ConnectionState

interface.go
@@ -264,7 +264,8 @@ type Config struct {
 
 // ConnectionState records basic details about a QUIC connection
 type ConnectionState struct {
-	TLS tls.ConnectionState
+	TLS               tls.ConnectionState
+	SupportsDatagrams bool
 }
 
 // A Listener for incoming QUIC connections

nxenon/xquic-go

interface.go
@@ -264,7 +264,8 @@ type Config struct {
 
 // ConnectionState records basic details about a QUIC connection
 type ConnectionState struct {
-	TLS handshake.ConnectionState
+	TLS               handshake.ConnectionState
+	SupportsDatagrams bool
 }
 
 // A Listener for incoming QUIC connections